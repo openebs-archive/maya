0396da80a8714e70b9280e3b7600d2d3a8bb706a
Prateek <prateekpandey14@gmail.com>
2017-03-09 17:46:29 +0530

    taking -self-ip as -bind ip

--- scripts/start_mayaserver.sh
+++ scripts/start_mayaserver.sh
@@ -2,10 +2,19 @@
 
 set -e
 
+SELF_IPV4=$1
+
+if [ $# -ne "" ]; then
+   SELF_IP4=127.0.0.1
+fi
+
 echo "Setup Mayaserver Daemon ..."
 
 # Place systemd service template for Mayaserver
 sudo cp /etc/maya.d/templates/mayaserver.service.tmpl /etc/systemd/system/mayaserver.service
+
+# Replace the placeholders with actual values
+sudo sed -e "s|__SELF_IPV4__|$SELF_IPV4|g" -i /etc/systemd/system/mayaserver.service 
 
 echo "Starting Mayaserver service ..."
 
