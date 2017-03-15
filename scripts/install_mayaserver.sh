7724d36973c87fce77404a0c458d4ee60ed16877
Prateek <prateekpandey14@gmail.com>
2017-03-15 15:37:58 +0530

    Bumping up the mayaserver version-0.0.5

--- scripts/install_mayaserver.sh
+++ scripts/install_mayaserver.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-MAYA_VERSION="0.0.4"
+MAYA_VERSION="0.0.5"
 CURDIR=`pwd`
 
 if [[ $(which mayaserver >/dev/null && mayaserver version | head -n 1 | cut -d ' ' -f 2 | sed 's/dev//' | cut -d "'" -f 2) == "$MAYA_VERSION" ]]; then    
