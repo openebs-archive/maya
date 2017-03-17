af88bfcf7005bfbccc259c91bb2d0e45ba1c5cc4
Prateek <prateekpandey14@gmail.com>
2017-03-17 14:09:01 +0530

    Handling Errors at run-time configuring

--- command/network-flannel.go
+++ command/network-flannel.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"fmt"
 	"os/exec"
 	"strings"
 )
@@ -21,12 +22,21 @@ type NetworkInstallCommand struct {
 
 func (c *NetworkInstallCommand) Help() string {
 	helpText := `
-	Usage: maya network-install <k8smaster-name> <ip-addr>
+	Usage: maya network-install <name> <ip>
+
+	Configure the virtual network for containers on OpenEBS Host (osh)
 
 Maya Network options:
-		
 
-		`
+  -name= <Name>
+    This is name of the kubernetes-master which is running
+    the etcd server to manage the key-value pair.
+ 
+  -ip= <IP Address> 
+    This args is ip-address of the same etcd server mentioned above 
+	running on kubernetes-master.
+
+`
 	return strings.TrimSpace(helpText)
 }
 
@@ -40,19 +50,37 @@ func (c *NetworkInstallCommand) Run(args []string) int {
 
 	flags := c.M.FlagSet("network-install", FlagSetClient)
 	flags.Usage = func() { c.M.Ui.Output(c.Help()) }
-	flags.StringVar(&c.kubename, "k8smaster-name", "", "")
-	flags.StringVar(&c.kube_ip, "ip-addr", "", "")
+	flags.StringVar(&c.kubename, "name", "", "")
+	flags.StringVar(&c.kube_ip, "ip", "", "")
 
 	if err := flags.Parse(args); err != nil {
 		return 1
 	}
 
-	//check the args
+	//check the non-flag arguments
 	args = flags.Args()
-	if len(args) > 1 {
+	if len(args) != 0 {
 		c.M.Ui.Error(c.Help())
 		return 1
 	}
+
+	//check the args
+	if len(strings.TrimSpace(c.kubename)) == 0 {
+		c.M.Ui.Error(fmt.Sprintf("-name option is mandatory\n"))
+		c.M.Ui.Error(c.Help())
+		return 1
+	}
+
+	if len(strings.TrimSpace(c.kube_ip)) == 0 {
+		c.M.Ui.Error(fmt.Sprintf("-ip option is mandatory\n"))
+		c.M.Ui.Error(c.Help())
+		return 1
+	}
+	//stdout the configuration
+	fmt.Printf("following Configuration has been passed:\n")
+	fmt.Printf("k8smaster-name = %v\n", c.kubename)
+	fmt.Printf("k8smaster-ip = %v\n", c.kube_ip)
+
 	if runop = c.installFlannel(); runop != 0 {
 		return runop
 	}
