9ca0716ad194bade19ea0bac75ff8a9f492d5752
Prateek <prateekpandey14@gmail.com>
2017-03-21 15:55:54 +0530

    Fixing typo

--- scripts/install_mayaserver.sh
+++ scripts/install_mayaserver.sh
@@ -11,12 +11,11 @@ fi
 
 cd /tmp/
 
-if [ ! -f "./mayaserver_${MAYA_VERSION}.zip" ]; then
+#if [ ! -f "./mayaserver_${MAYA_VERSION}.zip" ]; then
 echo "Fetching Mayaserver ${MAYA_VERSION} ..."
 #curl -sSL https://github.com/openebs/mayaserver/releases/download/${MAYA_VERSION}/mayaserver-linux_amd64.zip -o mayaserver.zip
 wget $(curl -sS $releaseurl | grep "browser_download_url" | awk '{print $2}' | tr -d '"' | head -n 2 | tail -n 1)
-exit
-fi
+
 echo "Installing Mayaserver ${MAYA_VERSION} ..."
 unzip mayaserver-linux_amd64.zip
 sudo chmod +x mayaserver
