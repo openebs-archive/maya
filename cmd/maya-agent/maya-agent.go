0698acb18c3916c269ce8b17cdf41929ebba0873
kmova <kiran.mova@cloudbyte.com>
2017-08-23 01:32:56 +0530

    #76 - fix travis failure - k8s.io/pkg referencing

--- cmd/maya-agent/maya-agent.go
+++ cmd/maya-agent/maya-agent.go
@@ -2,7 +2,8 @@ package main
 
 import "os"
 import "github.com/openebs/maya/cmd/maya-agent/app"
-import k8slogsutil "k8s.io/kubernetes/pkg/util/logs"
+
+//import k8slogsutil "k8s.io/kubernetes/pkg/util/logs"
 
 func main() {
 	if err := run(); err != nil {
@@ -14,8 +15,8 @@ func main() {
 // Run maya-agent
 func run() error {
 	// Init logging
-	k8slogsutil.InitLogs()
-	defer k8slogsutil.FlushLogs()
+	//k8slogsutil.InitLogs()
+	//defer k8slogsutil.FlushLogs()
 
 	// Create & execute new command
 	cmd, err := app.NewMayaAgent()
