d20adec20efdf59a73da88ee0eb282935fa053fe
amitkumardas <amit.das@cloudbyte.com>
2017-10-09 12:12:07 +0530

    simplify Vagrantfile to compile and build source
    1. Why is this change necessary ?
    
    The current Vagrantfile does a lot of things, like
    setting up a standalone storage cluster. This is no
    more relevant in this repo and setup related things
    has moved to openebs/openebs.
    
    2. How does this change address the issue ?
    
    - This file comments out the un-required steps
    
    3. How to verify this change ?
    
    - Vagrant status will list a single VM
    
    4. What side effects does this change have ?
    
    - None

---
+++ buildscripts/install_go.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Used by development setups especially Vagrant
+# NOTE: Use of vagrant user !!!
+set -ex
+
+GO_VERSION="1.9.1"
+CURDIR=`pwd`
+
+# Setup go, for development
+SRCROOT="/opt/go"
+SRCPATH="/opt/gopath"
+
+# Get the ARCH
+ARCH=`uname -m | sed 's|i686|386|' | sed 's|x86_64|amd64|'`
+
+# Install Go
+cd /tmp
+
+if [ ! -f "./go${GO_VERSION}.linux-${ARCH}.tar.gz" ]; then
+  wget -q https://storage.googleapis.com/golang/go${GO_VERSION}.linux-${ARCH}.tar.gz
+fi
+
+tar -xf go${GO_VERSION}.linux-${ARCH}.tar.gz
+sudo mv go $SRCROOT
+sudo chmod 775 $SRCROOT
+sudo chown vagrant:vagrant $SRCROOT
+
+# Setup the GOPATH; 
+# This allows subsequent "go get" commands to work.
+sudo mkdir -p $SRCPATH
+sudo chown -R vagrant:vagrant $SRCPATH 2>/dev/null || true
+# ^^ silencing errors here because we expect this to fail for the shared folder
+
+cat <<EOF >/tmp/gopath.sh
+export GOPATH="$SRCPATH"
+export GOROOT="$SRCROOT"
+export PATH="$SRCROOT/bin:$SRCPATH/bin:\$PATH"
+EOF
+
+sudo mv /tmp/gopath.sh /etc/profile.d/gopath.sh
+sudo chmod 0755 /etc/profile.d/gopath.sh
+source /etc/profile.d/gopath.sh
+
+cd ${CURDIR}
