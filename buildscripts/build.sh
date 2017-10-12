4a042d3748e92450bd2380009010be1c999aca09
Benjamin Misell <beninjam174@gmail.com>
2017-10-12 19:32:48 +0100

    Move from gox to go build

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -22,7 +22,9 @@ GIT_TAG="$(git describe --tags $(git rev-list --tags --max-count=1))"
 # Determine the arch/os combos we're building for
 XC_ARCH=${XC_ARCH:-"386 amd64"}
 XC_OS=${XC_OS:-"linux"}
-XC_EXCLUDE=${XC_EXCLUDE:-"!darwin/arm !darwin/386"}
+
+XC_ARCHS=(${XC_ARCH// / })
+XC_OSS=(${XC_OS// / })
 
 # Delete the old dir
 echo "==> Removing old directory..."
@@ -48,16 +50,21 @@ fi
 # Build!
 echo "==> Building..."
 
-gox \
-    -os="${XC_OS}" \
-    -arch="${XC_ARCH}" \
-    -osarch="${XC_EXCLUDE}" \
-    -ldflags \
-       "-X main.GitCommit='${GIT_COMMIT}${GIT_DIRTY}' \
-        -X main.MayaCtlName='${MAYACTL}' \
-        -X main.Version='${GIT_TAG}'" \
-    -output "bin/{{.OS}}_{{.Arch}}/${MAYACTL}" \
-    .
+for GOOS in "${XC_OSS[@]}"
+do
+    for GOARCH in "${XC_ARCHS[@]}"
+    do
+        output_name="bin/"$GOOS"_"$GOARCH"/"$MAYACTL
+
+        if [ $GOOS = "windows" ]; then
+            output_name+='.exe'
+        fi
+        echo $GOARCH
+        env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name
+
+    done
+
+done
 
 echo ""
 
