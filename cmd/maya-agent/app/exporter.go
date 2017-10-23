15a2292bce6c7c3804a8fc4d06285569afd56663
utkarshmani1997 <utkarshmani1997@gmail.com>
2017-10-23 15:59:02 +0530

    Merge openebs-exporter with maya-agent
    **What this PR does**:
    
    * This PR refers to issue
    * [#683](https://github.com/openebs/openebs/issues/683)
    * This PR merge openebs-exporter with maya-agent
    
    **why we need it**:
    
    * maya-agent will be used for monitoring purpose.
    
    **How this commit address the issue**:
    
    * a command `monitor` is added along with some flags which can be used
      to monitor volumes.e.g. `maya-agent monitor -c=http://localhost:9501`
    
    **How to verify this change**
    
    * run this binary locally or in maya-apiserver container and do
      `curl localhost:9500/metrics` and make sure volume is being
       consumed otherwise it will show zero in all the stats fields.
    
    * An [issue](https://github.com/openebs/openebs/issues/690) is created
      to write buildscripts and Dockerfile.

---
+++ cmd/maya-agent/app/exporter.go
@@ -0,0 +1,51 @@
+package app
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/openebs/maya/cmd/maya-agent/app/collector"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// Entrypoint is used to monitor OpenEBS volumes. It's used by maya-agent to
+// start an instance of openebs exporter.
+func Entrypoint(options *OpenEBSExporterOptions) {
+	controllerURL, err := url.Parse(options.ControllerAddress)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exporter := collector.NewExporter(controllerURL)
+	prometheus.MustRegister(exporter)
+
+	log.Printf("Starting Server: %s", options.ListenAddress)
+	if options.MetricsPath == "" || options.MetricsPath == "/" {
+
+		http.Handle(options.MetricsPath, promhttp.Handler())
+
+	} else {
+
+		http.Handle(options.MetricsPath, promhttp.Handler())
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			homepage := `<html>
+<head><title>OpenEBS Exporter</title></head>
+<body>
+<h1>OpenEBS Exporter</h1>
+<p><a href="` + options.MetricsPath + `">Metrics</a></p>
+</body>
+</html>
+`
+			w.Write([]byte(homepage))
+		})
+
+	}
+
+	err = http.ListenAndServe(options.ListenAddress, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ cmd/maya-agent/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/openebs/maya/cmd/maya-agent/app"
+	mayalogger "github.com/openebs/maya/kit/logs"
+)
+
+func main() {
+	if err := run(); err != nil {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+// Run maya-agent
+func run() error {
+	// Init logging
+	mayalogger.InitLogs()
+	defer mayalogger.FlushLogs()
+
+	// Create & execute new command
+	cmd, err := app.NewMayaAgent()
+	if err != nil {
+		return err
+	}
+
+	return cmd.Execute()
+}
