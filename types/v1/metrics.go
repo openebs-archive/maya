4a1e1a5d33b7b361cde5c8fee34d86656dbffa99
utkarshmani1997 <utkarshmani1997@gmail.com>
2017-10-23 22:20:22 +0530

    move struct types to types/v1

---
+++ types/v1/metrics.go
@@ -0,0 +1,24 @@
+package v1
+
+import "github.com/rancher/go-rancher/client"
+
+// OpenEBSVolumeMetrics is used to store the collected metrics
+// all the stats exposed by jiva stored into OpenEBSVolumeMetrics fields
+type OpenEBSVolumeMetrics struct {
+	Resource        client.Resource
+	RevisionCounter int64         `json:"RevisionCounter"`
+	ReplicaCounter  int64         `json:"ReplicaCounter"`
+	SCSIIOCount     map[int]int64 `json:"SCSIIOCount"`
+
+	ReadIOPS            string `json:"ReadIOPS"`
+	TotalReadTime       string `json:"TotalReadTime"`
+	TotalReadBlockCount string `json:"TotalReadBlockCount"`
+
+	WriteIOPS            string `json:"WriteIOPS"`
+	TotalWriteTime       string `json:"TotalWriteTime"`
+	TotalWriteBlockCount string `json:"TotatWriteBlockCount"`
+
+	UsedLogicalBlocks string `json:"UsedLogicalBlocks"`
+	UsedBlocks        string `json:"UsedBlocks"`
+	SectorSize        string `json:"SectorSize"`
+}
