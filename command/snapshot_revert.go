a6be54cbcc86f96bc72cdac0cefb265c33ce8526
Mehulkumar <m3hul.g4m3r@gmail.com>
2017-10-31 12:45:34 +0530

    Update: Fix linting errors (#776)

--- command/snapshot_revert.go
+++ command/snapshot_revert.go
@@ -8,46 +8,46 @@ import (
 
 // SnapshotRevertCommand struct for reverting snapshot of a Volume
 type SnapshotRevertCommand struct {
-    Meta
-    Name  string
-    Sname string
-    //client *ControllerClient
+	Meta
+	Name  string
+	Sname string
+	//client *ControllerClient
 }
 
 // Help shows usage of `maya snapshot revert` command
 func (s *SnapshotRevertCommand) Help() string {
-    helpText := `
+	helpText := `
     Usage: maya snapshot revert -volname <vol> -snapname <snap>
 
     This command will revert to specific snapshot of a Volume.
 
     `
-    return strings.TrimSpace(helpText)
+	return strings.TrimSpace(helpText)
 }
 
 // Synopsis shows short information related to CLI command
 func (s *SnapshotRevertCommand) Synopsis() string {
-    return "Reverts to specific snapshot of a Volume"
+	return "Reverts to specific snapshot of a Volume"
 }
 
 // Run runs `maya snapshot revert` command
 func (s *SnapshotRevertCommand) Run(args []string) int {
-    flags := s.Meta.FlagSet("vsm-snapshot", FlagSetClient)
-    flags.Usage = func() { s.Ui.Output(s.Help()) }
+	flags := s.Meta.FlagSet("vsm-snapshot", FlagSetClient)
+	flags.Usage = func() { s.Ui.Output(s.Help()) }
 
-    flags.StringVar(&s.Name, "volname", "", "")
-    flags.StringVar(&s.Sname, "snapname", "", "")
+	flags.StringVar(&s.Name, "volname", "", "")
+	flags.StringVar(&s.Sname, "snapname", "", "")
 
-    if err := flags.Parse(args); err != nil {
-        return 1
-    }
-    var c *ControllerClient
-    if err := c.RevertSnapshot(s.Name, s.Sname); err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to revert snapshot %s: %v\n", s.Sname, err)
-        return 1
-    }
-    fmt.Println("Snapshot reverted:", s.Sname)
-    return 0
+	if err := flags.Parse(args); err != nil {
+		return 1
+	}
+	var c *ControllerClient
+	if err := c.RevertSnapshot(s.Name, s.Sname); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to revert snapshot %s: %v\n", s.Sname, err)
+		return 1
+	}
+	fmt.Println("Snapshot reverted:", s.Sname)
+	return 0
 }
 
 // RevertSnapshot reverts given volume to given snapshot
