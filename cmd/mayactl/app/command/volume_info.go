59187b8257ffe39972694069b430444fe26806e6
Sumit Lalwani <sumit.lalwani97@gmail.com>
2018-06-04 12:47:11 +0530

    Improve template view in mayactl volume info command
    Signed-off-by: Sumit Lalwani <sumit.lalwani97@gmail.com>

--- cmd/mayactl/app/command/volume_info.go
+++ cmd/mayactl/app/command/volume_info.go
@@ -146,17 +146,10 @@ func (c *CmdVolumeInfoOptions) DisplayVolumeInfo(a *Annotations, collection clie
 	)
 	const (
 		replicaTemplate = `
-<<<<<<< 47d72fc44065a7693c2b53a6ddd45a234114afdf
 		
 Replica Details : 
 ---------------- {{range $key, $value := .}}
 {{ printf "%s\t" $value.Name }} {{ printf "%s\t" $value.AccessMode }} {{ printf "%s\t" $value.Status }} {{ printf "%s\t" $value.IP }} {{ $value.NodeName }} {{end}}
-=======
-============================================== Replica Details ==============================================
-{{range $key, $value := .}}
-{{ printf "%s\t" $value.Name }} {{ printf "%s\t" $value.AccessMode }} {{ printf "%s\t" $value.Status }} {{ printf "%s\t" $value.IP }} {{ $value.NodeName }} {{end}}
-=============================================================================================================
->>>>>>> Proper alignment with the use of tabwriter
 `
 		portalTemplate = `
 Portal Details : 
@@ -217,7 +210,6 @@ Status  :   {{.Status}}
 		address = append(address, strings.TrimSuffix(strings.TrimPrefix(collection.Data[key].Address, "tcp://"), v1.ReplicaPort))
 		mode = append(mode, collection.Data[key].Mode)
 		replicaIPStatus[address[key]].mode = mode[key]
-
 	}
 
 	for k, v := range replicaIPStatus {
@@ -234,22 +226,10 @@ Status  :   {{.Status}}
 		fmt.Println("Error in getting information from K8s. Please try again")
 	}
 
-<<<<<<< 47d72fc44065a7693c2b53a6ddd45a234114afdf
 	tmpl = template.New("ReplicaInfo")
 	tmpl = template.Must(tmpl.Parse(replicaTemplate))
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
-=======
-	if length < replicaCount {
-		for i := length; i < (replicaCount); i++ {
-			replicaInfo[i+1] = &ReplicaInfo{"NA", "NA", replicaStatus[i], "NA", "NA"}
-		}
-	}
-	tmpl = template.New("ReplicaInfo")
-	tmpl = template.Must(tmpl.Parse(replicaTemplate))
-
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 8, ' ', 0)
->>>>>>> Proper alignment with the use of tabwriter
 	err = tmpl.Execute(w, replicaInfo)
 	if err != nil {
 		fmt.Println("Unable to display volume info, found error : ", err)
