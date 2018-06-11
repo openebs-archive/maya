ae8653453d30c06f8d0dd61aae934645403680be
Sumit Lalwani <sumit.lalwani97@gmail.com>
2018-06-11 17:28:50 +0530

    Add Parent and Children detail in snapshot list and update unit test
    Signed-off-by: Sumit Lalwani <sumit.lalwani97@gmail.com>

--- pkg/client/jiva/model.go
+++ pkg/client/jiva/model.go
@@ -17,6 +17,8 @@ const (
 	diskSuffix         = ".img"
 	diskName           = diskPrefix + "%s" + diskSuffix
 	maximumChainLength = 250
+	snapPrefix         = "volume-snap-"
+	snapSuffix         = ".img"
 )
 
 type Volumes struct {
@@ -164,9 +166,23 @@ func Contains(arr []string, val string) bool {
 	return false
 }
 
+// IsHeadDisk checks if Disk is a Head
 func IsHeadDisk(diskName string) bool {
 	if strings.HasPrefix(diskName, headPrefix) && strings.HasSuffix(diskName, headSuffix) {
 		return true
 	}
 	return false
+}
+
+// TrimSnapshotName trims the snapshot name from diskName string
+func TrimSnapshotName(diskName string) string {
+	if diskName == "" {
+		return "NA"
+	} else if strings.HasPrefix(diskName, headPrefix) && strings.HasSuffix(diskName, headSuffix) {
+		return strings.TrimSuffix(strings.TrimPrefix(diskName, "volume-"), ".img")
+	} else if strings.HasPrefix(diskName, snapPrefix) && strings.HasSuffix(diskName, snapSuffix) {
+		return strings.TrimSuffix(strings.TrimPrefix(diskName, "volume-snap-"), ".img")
+	}
+
+	return "NA"
 }
