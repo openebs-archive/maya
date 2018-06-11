843bbf0cef1426a025a452b438de82845c2cd061
moteesh <moteesh@gmail.com>
2018-06-11 16:16:22 +0530

    log message improved according to review comments
    Signed-off-by: moteesh <moteesh@gmail.com>

--- cmd/cstor-volume-mgmt/controller/volume-controller/run_volume_controller.go
+++ cmd/cstor-volume-mgmt/controller/volume-controller/run_volume_controller.go
@@ -89,7 +89,7 @@ func (c *CStorVolumeController) processNextWorkItem() bool {
 		// Finally, if no error occurs we Forget this item so it does not
 		// get queued again until another change happens.
 		c.workqueue.Forget(obj)
-		glog.Infof("Successfully synced '%s'", q.Key)
+		glog.Infof("Successfully synced Key : '%s', Operation : '%s'", q.Key, q.Operation)
 		return nil
 	}(obj)
 
