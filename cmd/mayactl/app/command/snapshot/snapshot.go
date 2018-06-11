af1100a5b9a905f15b0896ab8af53268db34d201
Prateek Pandey <prateekpandey14@gmail.com>
2018-06-11 18:49:04 +0530

    Fix mayactl snapshot command helptext (#366)
    * Adds help text for snapshot
    
    Signed-off-by: naveen kumar <kumarnjnv22@gmail.com>

--- cmd/mayactl/app/command/snapshot/snapshot.go
+++ cmd/mayactl/app/command/snapshot/snapshot.go
@@ -32,11 +32,42 @@ type CmdSnaphotOptions struct {
 	namespace string
 }
 
+var (
+	snapshotCommandHelpText = `
+Command provides operations related to a volume snapshot.
+
+If you have specified an OpenEBS volume in a namespace other than the 'default',
+you must use --namespace flag with a command. If not, you will see the results only
+in the 'default' namespace.
+
+Usage: mayactl snapshot <subcommand> [options] [args]
+
+Examples:
+  # Create a snapshot:
+    $ mayactl snapshot create --volname <vol> --snapname <snap>
+
+  # Create a snapshot for a volume created in 'test' namespace
+    $ mayactl snapshot create --volname <vol> --snapname <snap> --namespace test
+
+  # Lists snapshot:
+    $ mayactl snapshot list --volname <vol>
+
+  # Lists snapshots for a volume created in 'test' namespace
+    $ mayactl snapshot list --volname <vol> --namespace test
+
+  # Reverts a snapshot:
+    $ mayactl snapshot revert --volname <vol> --snapname <snap>
+
+  # Revert a snapshot for a volume created in 'test' namespace
+    $ mayactl snapshot revert --volname <vol> --snapname <snap> --namespace test
+`
+)
+
 func NewCmdSnapshot() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "snapshot",
-		Short: "Provides operations related to snapshot of a Volume",
-		Long:  "Provides operations related to snapshot of a Volume",
+		Short: "Provides operations related to a Volume snapshot",
+		Long:  snapshotCommandHelpText,
 	}
 
 	cmd.AddCommand(
