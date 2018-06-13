fdb1bbe62e6786ad75994e5e27fb75288812145a
prateekpandey14 <prateekpandey14@gmail.com>
2018-06-13 20:59:47 +0530

    Hardening Volume clone API
    Enhance volume create API to get source volume controller-ip
    and storageclass information to create clone of volume.
    
    Signed-off-by: prateekpandey14 <prateekpandey14@gmail.com>

--- orchprovider/k8s/v1/k8s.go
+++ orchprovider/k8s/v1/k8s.go
@@ -478,7 +478,6 @@ func (k *k8sOrchestrator) readVSM(vsm string, volProProfile volProfile.VolumePro
 	if err != nil {
 		return nil, err
 	}
-
 	glog.Infof("Fetching info on volume '%s: %s'", ns, vsm)
 
 	//annotations := map[string]string{}
@@ -576,16 +575,13 @@ func (k *k8sOrchestrator) readVSM(vsm string, volProProfile volProfile.VolumePro
 	if !doesExist {
 		return nil, nil
 	}
-
 	mb.AddIQN(vsm)
-
 	// TODO
 	// This is a temporary type that is used
 	// Will move to VSM type
 	pv := &v1.Volume{}
 	pv.Name = vsm
 	pv.Annotations = mb.AsAnnotations()
-
 	if mb.IsVolumeRunning(pv) {
 		pv.Status.Phase = v1.VolumePhase(v1.VolumeRunningVV)
 	} else {
@@ -828,7 +824,6 @@ func (k *k8sOrchestrator) createControllerDeployment(volProProfile volProfile.Vo
 	if err != nil {
 		return nil, err
 	}
-
 	glog.Infof("Adding controller for volume 'name: %s'", vsm)
 	var tolerationSeconds int64 = 0
 
@@ -840,6 +835,7 @@ func (k *k8sOrchestrator) createControllerDeployment(volProProfile volProfile.Vo
 				string(v1.PVCSelectorKey):               pvc,
 				string(v1.VolumeProvisionerSelectorKey): string(v1.JivaVolumeProvisionerSelectorValue),
 				string(v1.ControllerSelectorKey):        string(v1.JivaControllerSelectorValue),
+				string(v1.VolumeStorageClassVK):         string(vol.Labels.K8sStorageClass),
 			},
 		},
 		TypeMeta: metav1.TypeMeta{
@@ -932,6 +928,7 @@ func (k *k8sOrchestrator) createControllerDeployment(volProProfile volProfile.Vo
 	// values. Labels may be considered for setting values.
 	mg := NewVolumeMarkerBuilder()
 	mg.AddMonitoringPolicy(vol.Monitor)
+	mg.AddStorageClass(vol.Labels.K8sStorageClass)
 	mg.AddVolumeType(string(vol.VolumeType))
 
 	deploy.Annotations = mg.AsAnnotations()
@@ -1017,6 +1014,7 @@ func (k *k8sOrchestrator) createReplicaDeployment(volProProfile volProfile.Volum
 				string(v1.ReplicaSelectorKey):           string(v1.JivaReplicaSelectorValue),
 				// -- if manual replica addition
 				//string(v1.ReplicaCountSelectorKey):      strconv.Itoa(rCount),
+				string(v1.VolumeStorageClassVK): string(vol.Labels.K8sStorageClass),
 			},
 		},
 		TypeMeta: metav1.TypeMeta{
