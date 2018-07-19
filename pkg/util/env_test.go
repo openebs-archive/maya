c4bfc9bcc993079ab9c0192962ce391096e070fa
Prince Rachit Sinha <prince.rachit@mayadata.io>
2018-07-19 16:15:22 +0530

    remove assert lib, add more testcases
    Signed-off-by: Prince Rachit Sinha <prince.rachit@mayadata.io>

--- pkg/util/env_test.go
+++ pkg/util/env_test.go
@@ -3,12 +3,13 @@ package util
 import (
 	"testing"
 	"os"
-	"github.com/magiconair/properties/assert"
+	"reflect"
 )
 
 func TestLookEnv(t *testing.T) {
 	// set required environment
 	os.Setenv("_MY_PRESENT_TEST_KEY_", "value1")
+	os.Setenv("_MY_PRESENT_TEST_KEY_W_EMPTY_VALUE", "")
 	os.Unsetenv("_MY_MISSING_TEST_KEY_") // for safety
 
 	cases := map[string]struct {
@@ -23,21 +24,38 @@ func TestLookEnv(t *testing.T) {
 			key:         "_MY_PRESENT_TEST_KEY_",
 			expectValue: "value1",
 		},
+		"Present env variable with empty value": {
+			key:         "_MY_PRESENT_TEST_KEY_W_EMPTY_VALUE",
+			expectValue: "",
+		},
 	}
 
 	for k, v := range cases {
 		t.Run(k, func(t *testing.T) {
-			assert.Equal(t, lookEnv(v.key), v.expectValue)
+			actualValue := lookEnv(v.key)
+			if !reflect.DeepEqual(actualValue, v.expectValue) {
+				t.Errorf("expected %s got %s", v.expectValue, actualValue)
+			}
 		})
 	}
 }
 
 func TestCASTemplateFeatureGate(t *testing.T) {
+	// avoiding table driven parallel tests as tests depend
+	// on hosts environment variable
+
 	// negative testcase, incorrect value
 	os.Setenv(string(CASTemplateFeatureGateENVK), "on")
 	_, err := CASTemplateFeatureGate()
 	if err == nil {
 		t.Errorf("expected error when feature gate %s is %s", CASTemplateFeatureGateENVK, "on")
+	}
+
+	// negative testcase, empty value
+	os.Setenv(string(CASTemplateFeatureGateENVK), "")
+	_, err = CASTemplateFeatureGate()
+	if err == nil {
+		t.Errorf("expected error when feature gate %s is empty", CASTemplateFeatureGateENVK)
 	}
 
 	// Positive testcase, absent env variable
