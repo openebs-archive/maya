c007d912c85013c09170d39bb050f793878d27f6
Ashutosh Kumar <sonasingh46@gmail.com>
2018-07-31 21:47:21 +0530

    Add test for controller instance in spc controller (#418)
    This commit will add test for controller instance
    of spc watcher.
    
    Signed-off-by: sonasingh46 <sonasingh46@gmail.com>

--- cmd/maya-apiserver/spc-watcher/controller_test.go
+++ cmd/maya-apiserver/spc-watcher/controller_test.go
@@ -20,7 +20,17 @@ import (
 	apis "github.com/openebs/maya/pkg/apis/openebs.io/v1alpha1"
 	api_meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"time"
+	openebsFakeClientset "github.com/openebs/maya/pkg/client/clientset/versioned/fake"
+	informers "github.com/openebs/maya/pkg/client/informers/externalversions"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes/fake"
 )
+
+// TestIsDeleteEvent function tests IsDeleteEvent function
+
+// IsDeleteEvent function takes a kubernetes object as argument
+// and returns a boolean value which tells whether the object is
+// scheduled for deletion or not
 
 func TestIsDeleteEvent(t *testing.T) {
 
@@ -51,5 +61,36 @@ func TestIsDeleteEvent(t *testing.T) {
 				t.Errorf("Test case failed: expected '%v' but got '%v' ", test.expectedResult, result)
 			}
 		})
+	}
+}
+
+// TestNewCStorPoolController function tests if the kubernetes, openebs clientsets
+// and other required objects are present in controller instance.
+
+func TestNewCStorPoolController(t *testing.T) {
+	fakeKubeClient := fake.NewSimpleClientset()
+	fakeOpenebsClient := openebsFakeClientset.NewSimpleClientset()
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(fakeKubeClient, time.Second*30)
+	openebsInformerFactory := informers.NewSharedInformerFactory(fakeOpenebsClient, time.Second*30)
+	// Instantiate the controller
+	controller := NewController(fakeKubeClient, fakeOpenebsClient, kubeInformerFactory,
+		openebsInformerFactory)
+
+	if controller.spcSynced == nil{
+		t.Errorf("No spc cache sync in controller object")
+	}
+
+	if controller.workqueue == nil{
+		t.Errorf("No workqueue in controller object")
+	}
+	if controller.recorder == nil{
+		t.Errorf("No recorder in controller object")
+	}
+	if controller.kubeclientset != fakeKubeClient {
+		t.Errorf("SPC controller object's kubeclientset mismatch")
+	}
+	if controller.clientset != fakeOpenebsClient {
+		t.Errorf("SPC controller object's openebsclientset mismatch")
 	}
 }
