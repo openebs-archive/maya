bff96a6eaf785b231243fcdf575a13a7b212d718
prateekpandey14 <prateekpandey14@gmail.com>
2018-08-26 21:32:41 +0530

    Remove unutilized scripts and templates directory
    This commit remove scripts and templates dir which are
    no longer needed.
    
    Signed-off-by: prateekpandey14 <prateekpandey14@gmail.com>

--- scripts/start_etcd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Starting etcd service ..."
-
-sudo systemctl daemon-reload
-sudo systemctl enable etcd.service
-sudo systemctl restart etcd.service
--- scripts/start_nomad_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Starting nomad server service ..."
-
-sudo systemctl enable nomad-server.service
-sudo systemctl restart nomad-server.service
