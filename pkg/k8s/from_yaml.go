76fd85d789781c8b7d9c56c8be547f209abeb1ef
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-10-02 22:43:30 +0800

    Improved comment documentation in package k8s
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- pkg/k8s/from_yaml.go
+++ pkg/k8s/from_yaml.go
@@ -14,7 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package k8s defines all the k8s related codebase.
+// Package k8s provides the necessary codebase for K8s implementation.
+// Setups the different yaml files for deployment
+
 package k8s
 
 import (
