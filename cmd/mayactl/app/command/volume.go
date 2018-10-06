2c2b527e3dbfc31f413085fa05b6f7b34207bb6c
Mukesh A <amukesh.mk@gmail.com>
2018-10-06 13:48:40 +0530

    feat(issue-1992) : improved mayactl error message: fixes #1992
    Signed-off-by: Mukesh A <amukesh.mk@gmail.com>

--- cmd/mayactl/app/command/volume.go
+++ cmd/mayactl/app/command/volume.go
@@ -136,7 +136,7 @@ func (c *CmdVolumeOptions) Validate(cmd *cobra.Command, snapshotnameverify, sour
 	}
 	if volnameverify {
 		if len(c.volName) == 0 {
-			return errors.New("--volname is missing. Please specify a unique volumename")
+			return errors.New("--volname not specified.")
 		}
 	}
 	return nil
