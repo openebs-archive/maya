1fd0662f6866a2a6e3f99cbe3c7090a8ae5f71a4
Ashutosh Kumar <sonasingh46@gmail.com>
2018-10-07 20:00:19 +0530

    [TA3152]refactor(spc-watcher):use the lease logic for both create and update SPC events (#523)
    Refactored the SPC lease acquisition logic to be used for both create and update events. 
    
    Signed-off-by: sonasingh46 <sonasingh46@gmail.com>

--- cmd/maya-apiserver/spc-watcher/lease.go
+++ cmd/maya-apiserver/spc-watcher/lease.go
@@ -13,91 +13,56 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package spc
 
 import (
-	"fmt"
-	apis "github.com/openebs/maya/pkg/apis/openebs.io/v1alpha1"
 	clientset "github.com/openebs/maya/pkg/client/generated/clientset/internalclientset"
-	menv "github.com/openebs/maya/pkg/env/v1alpha1"
-	"k8s.io/apimachinery/pkg/util/runtime"
-	"strings"
+	"k8s.io/client-go/kubernetes"
 )
 
-const (
-	// SpcLeaseKey is the key that will be used to acquire lease on spc object.
-	// It will be present in spc annotations
-	// If key has an empty value, that means no one has acquired a lease on spc object.
-	SpcLeaseKey = "openebs.io/spc-create-lease"
-)
+// ToDo : Move this file to pkg
 
-// Leases is an interface which assists in getting and releasing lease over an spc object
-type Leases interface {
-	// GetLease will try to get a lease on spc, in case of failure it will return error
-	GetLease() (string, error)
-	// UpdateLease will update the lease value of the spc
-	UpdateLease(leaseValue string) (*apis.StoragePoolClaim, error)
-	// RemoveLease will remove the acquired lease on the spc
-	RemoveLease() *apis.StoragePoolClaim
+// LeaseContract struct will be used as a value of lease key that will
+// give information about an acquired lease on object
+// The struct object will be parsed to string which will be then
+// put as a value to the lease key of object annotation.
+type LeaseContract struct {
+	// Holder is the namespace/name of the pod who acquires the lease
+	Holder           string `json:"holder"`
+	// LeaderTransition is the count of lease that has been taken on the object
+	// in its lifetime.
+	// e.g. One of the pod takes a lease on the object and release and then some other
+	// pod (or even the same pod ) takes a lease again on the object its leaderTransition
+	// value is 1.
+	// If an object has leaderTranisiton value equal to 'n' that means it was leased
+	// 'n+1' times in its lifetime by distinct, same or some distinct and some same pods.
+	LeaderTransition int    `json:"leaderTransition"`
+	// More specific details can be added here that will describe the
+	// current state of lease in more details.
+	// e.g. acquiredTimeStamp, self-release etc
+	// acquiredTimeStamp will tell when the lease was acquired
+	// self-release will tell whether the lease was removed by the acquirer or not
 }
 
-// spcLease is the struct which will implement the Leases interface
-type spcLease struct {
-	// spcObject is the storagepoolclaim object over which lease is to be taken
-	spcObject *apis.StoragePoolClaim
-	// leaseKey is lease key on current storagepoolclaim object
+// Leaser is an interface which assists in getting and releasing lease on an object
+type Leaser interface {
+	// Hold will try to get a lease, in case of failure it will return error
+	Hold() error
+	// Update will update the lease value on the object
+	Update(leaseValue string) error
+	// Release will remove the acquired lease on the object
+	Release()
+}
+
+// Lease is the struct which will implement the Leases interface
+type Lease struct {
+	// Object is the object over which lease is to be taken
+	Object interface{}
+	// leaseKey is lease key on object
 	leaseKey string
 	// oecs is the openebs clientset
 	oecs clientset.Interface
-}
-
-func (sl *spcLease) GetLease() (string, error) {
-	// Get the lease value.
-	leaseValue := sl.spcObject.Annotations[sl.leaseKey]
-	// If leaseValue is empty acquire lease.
-	if strings.TrimSpace(leaseValue) == "" {
-		spcObject, err := sl.UpdateLease(sl.getPodName())
-		if err != nil {
-			return "", err
-		}
-		return spcObject.Annotations[sl.leaseKey], nil
-	}
-	// If leaseValue is not empty, lease cannot be acquired.
-	return "", fmt.Errorf("lease on spc already acquired")
-}
-
-func (sl *spcLease) UpdateLease(leaseValue string) (*apis.StoragePoolClaim, error) {
-	newSpcObject := sl.spcObject
-	if newSpcObject.Annotations == nil {
-		// make a map that should contain the lease key in spc
-		mapLease := make(map[string]string)
-
-		// Fill the map lease key with lease value
-		mapLease[sl.leaseKey] = leaseValue
-		newSpcObject.Annotations = mapLease
-
-	} else {
-		newSpcObject.Annotations[sl.leaseKey] = leaseValue
-	}
-	spcObject, err := sl.oecs.OpenebsV1alpha1().StoragePoolClaims().Update(sl.spcObject)
-	if err != nil {
-		return nil, err
-	}
-	return spcObject, nil
-}
-
-// Can be used for reconcile loop use cases
-// TODO Remove using patch instead of update
-func (sl *spcLease) RemoveLease() *apis.StoragePoolClaim {
-	spcObject, err := sl.UpdateLease("")
-	if err != nil {
-		runtime.HandleError(fmt.Errorf("Lease could not be removed:%v", err))
-	}
-	return spcObject
-}
-
-func (sl *spcLease) getPodName() string {
-	podName := menv.Get(menv.OpenEBSMayaPodName)
-	podNameSpace := menv.Get(menv.OpenEBSNamespace)
-	return podNameSpace + "/" + podName
+	// kubeclientset is a standard kubernetes clientset
+	kubeclientset kubernetes.Interface
 }
