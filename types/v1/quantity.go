e6e705c0ec02337c49635097553ca5bc11dbceb7
pandyamarut <pandyamarut@gmail.com>
2018-10-13 11:54:40 +0530

    Adds the missing comment.
    fixes#2147
    
    Signed-off-by: pandyamarut <pandyamarut@gmail.com>

--- types/v1/quantity.go
+++ types/v1/quantity.go
@@ -13,7 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// This is extracted from k8s.io/apimachinery/pkg/api/resource/quantity.go
+// Package v1 is extracted from k8s.io/apimachinery/pkg/api/resource/quantity.go
+
 package v1
 
 import (
