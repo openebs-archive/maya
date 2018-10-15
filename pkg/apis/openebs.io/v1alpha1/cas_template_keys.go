73224dbd9bdc8a673e5d33fde50dff39fb24c9f6
Tushcr7 <k7tushar@gmail.com>
2018-10-15 19:55:33 +0000

    removed the package comment from cas_template_keys.go
    Signed-off-by: Tushcr7 <k7tushar@gmail.com>

--- pkg/apis/openebs.io/v1alpha1/cas_template_keys.go
+++ pkg/apis/openebs.io/v1alpha1/cas_template_keys.go
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1alpha1
 package v1alpha1
 
 // CasKey is used for all cas specific annotation keys
