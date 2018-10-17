365db82d898f189c660a5bae974edf3dfde8c785
Abhishek Gupta <gupta.abhishek0819@gmail.com>
2018-10-18 01:23:31 +0530

    [WIP]Fix #2139 golint issues area/maya
    Signed-off-by: Abhishek Gupta <gupta.abhishek0819@gmail.com>

--- cmd/maya-apiserver/app/command/start.go
+++ cmd/maya-apiserver/app/command/start.go
@@ -94,7 +94,7 @@ type CmdStartOptions struct {
 	httpServer *server.HTTPServer
 }
 
-// NewCmdStart creates start command for maya-apiserver.
+// NewCmdStart creates start command for maya-apiserver
 func NewCmdStart() *cobra.Command {
 	options := CmdStartOptions{}
 
