5e72c6c23d1bb994362752e451438411a665ee07
galihmelon <galihmelon@gmail.com>
2018-10-17 21:09:13 +0100

    Remove unnecessary comment
    to address comment from PR review
    Signed-off-by: galihmelon <galihmelon@gmail.com>

--- types/v1/amount.go
+++ types/v1/amount.go
@@ -13,7 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1 is extracted from k8s.io/apimachinery/pkg/api/resource/amount.go
 package v1
 
 import (
