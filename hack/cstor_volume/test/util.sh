d242b7ef7049d38f96caecc87d9dc261c2867695
Mohd Feroz Baig <feroz.baig96@gmail.com>
2018-11-09 11:27:54 +0530

    refactor(util.sh): removal of duplicate code in hack/cstor_volume/test/util.sh (#731)
    * refactor: removal of duplicate code in hack/cstor_volume/test/util.sh
    
    * Update util.sh: Added comment
    
    Signed-off-by: Feroz Baig <feroz.baig96@gmail.com>

--- hack/cstor_volume/test/util.sh
+++ hack/cstor_volume/test/util.sh
@@ -1,28 +1,30 @@
 #!bin/bash
 # kubectl related generic functions
-kubectlApply()
+
+prefixedFileNames()
 {
     # append all the files together prefixed with -f
-    filesToApply=""
+    local filesToApply=""
     while [ "$1" != "" ]; do
         filesToApply="$filesToApply -f $1"
         sleep 1
         shift
     done
 
+    # printing the following for N no of files passed as argument
+    # -f file1 -f file2 -f file3 ... -f fileN
+    echo $filesToApply
+}
+
+kubectlApply()
+{
+    filesToApply=$(prefixedFileNames $@)
     kubectl apply $filesToApply
 }
 
 kubectlDelete()
 {
-    # append all the files together prefixed with -f
-    filesToDelete=""
-    while [ "$1" != "" ]; do
-        filesToDelete="$filesToDelete -f $1"
-        sleep 1
-        shift
-    done
-
+    filesToDelete=$(prefixedFileNames $@)
     kubectl delete $filesToDelete
 }
 
