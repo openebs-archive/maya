4246b6a6a8c399d70ab3285298a4e4a449b01c9e
Ashish Ranjan <ashishranjan738@gmail.com>
2018-11-28 13:06:46 +0530

    refactor(ci): changed make generated_files to make kubegen
    Signed-off-by: Ashish Ranjan <ashishranjan738@gmail.com>

--- buildscripts/travis-build.sh
+++ buildscripts/travis-build.sh
@@ -43,10 +43,10 @@ rc=$?; if [[ $rc != 0 ]]; then echo "make format failed"; exit $rc; fi
 checkGitDiff "make format"
 printf "\n"
 
-echo "Running : make generated_files"
-make generated_files
-rc=$?; if [[ $rc != 0 ]]; then echo "make generated_files"; exit $rc; fi
-checkGitDiff "make generated_files"
+echo "Running : make kubegen"
+make kubegen
+rc=$?; if [[ $rc != 0 ]]; then echo "make kubegen failed"; exit $rc; fi
+checkGitDiff "make kubegen"
 printf "\n"
 
 ./buildscripts/test-cov.sh
