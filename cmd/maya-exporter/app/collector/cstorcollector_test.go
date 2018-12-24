6f92d015ea64d5c1e4b9f4819cae9c85ae8d4848
Utkarsh Mani Tripathi <utkarsh.tripathi@mayadata.io>
2018-12-24 11:25:12 +0530

    fix(exporter): change return order
    Signed-off-by: Utkarsh Mani Tripathi <utkarsh.tripathi@mayadata.io>

--- cmd/maya-exporter/app/collector/cstorcollector_test.go
+++ cmd/maya-exporter/app/collector/cstorcollector_test.go
@@ -72,7 +72,7 @@ func TestNewResponse(t *testing.T) {
 	}
 	for name, tt := range cases {
 		t.Run(name, func(t *testing.T) {
-			_, got := newResponse(tt.response)
+			got, _ := newResponse(tt.response)
 			if !reflect.DeepEqual(got, tt.output) {
 				t.Fatalf("unmarshal(%v) : expected %v, got %v", tt.response, tt.output, got)
 			}
