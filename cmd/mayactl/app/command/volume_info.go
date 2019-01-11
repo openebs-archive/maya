27c2af032b6195f15441d0a2828377414b67046a
Ashish Ranjan <ashishranjan738@gmail.com>
2019-01-11 18:58:16 +0530

    enhance(mayactl): add application information in mayactl
    Signed-off-by: Ashish Ranjan <ashishranjan738@gmail.com>
    
    This commit adds application details in mayactl volume describe command which will be very useful in finding which application is using the volume.
    
    Note: The default is N/A if information is not present
    
    Output of mayactl volume describe:
    
    When application information is present in response:
    Portal Details :
    ----------------
    IQN               :   iqn.2016-09.com.openebs.cstor:pvc-4be46c41-15a1-11e9-a262-b4b686bd0cff
    Volume            :   pvc-4be46c41-15a1-11e9-a262-b4b686bd0cff
    Portal            :   10.0.0.160:3260
    Size              :   5G
    Controller Status :   running,running,running
    Controller Node   :   127.0.0.1
    Replica Count     :   1
    
    Application Details:
    --------------------
    Application Pod Name: percona-598c987dc8-mdsnw
    Application Pod Namespace: app-percona-ns
    
    Replica Details :
    -----------------
    NAME                                                                STATUS      POOL NAME                  NODE
    ----                                                                ------      ---------                  -----
    pvc-4be46c41-15a1-11e9-a262-b4b686bd0cff-cstor-sparse-pool-4lxy     Running     cstor-sparse-pool-4lxy     127.0.0.1
    
    When application information is not present in response:
    Portal Details :
    ----------------
    IQN               :   iqn.2016-09.com.openebs.cstor:pvc-55154c45-15a1-11e9-a262-b4b686bd0cff
    Volume            :   pvc-55154c45-15a1-11e9-a262-b4b686bd0cff
    Portal            :   10.0.0.18:3260
    Size              :   5G
    Controller Status :   running,running,running
    Controller Node   :   127.0.0.1
    Replica Count     :   1
    
    Application Details:
    --------------------
    Application Pod Name: N/A
    Application Pod Namespace: N/A
    
    Replica Details :
    -----------------
    NAME                                                                STATUS      POOL NAME                  NODE
    ----                                                                ------      ---------                  -----
    pvc-55154c45-15a1-11e9-a262-b4b686bd0cff-cstor-sparse-pool-4lxy     Running     cstor-sparse-pool-4lxy     127.0.0.1

--- cmd/mayactl/app/command/volume_info.go
+++ cmd/mayactl/app/command/volume_info.go
@@ -78,6 +78,12 @@ type cstorReplicaInfo struct {
 	IP         string
 }
 
+// applicationsDetails stores information about the application connected to the volume
+type applicationsDetails struct {
+	PodName      string
+	PodNamespace string
+}
+
 // NewCmdVolumeInfo displays OpenEBS Volume information.
 func NewCmdVolumeInfo() *cobra.Command {
 	cmd := &cobra.Command{
@@ -186,6 +192,13 @@ Replica Details :
 {{ printf "%s\t" $value.Name }} {{ printf "%s\t" $value.Status }} {{ printf "%s\t" $value.PoolName }} {{ $value.NodeName }} {{end}}
 `
 
+		applicationInfoTemplate = `
+Application Details:
+--------------------
+Application Pod Name: {{.PodName}}
+Application Pod Namespace: {{.PodNamespace}}
+`
+
 		portalTemplate = `
 Portal Details :
 ----------------
@@ -208,6 +221,12 @@ Replica Count     :   {{.ReplicaCount}}
 		v.GetReplicaCount(),
 		v.GetControllerNode(),
 	}
+
+	applicationInfo := applicationsDetails{
+		PodName:      v.Volume.ObjectMeta.Annotations["openebs.io/application-pod-name"],
+		PodNamespace: v.Volume.ObjectMeta.Annotations["openebs.io/application-pod-namespace"],
+	}
+
 	tmpl, err := template.New("VolumeInfo").Parse(portalTemplate)
 	if err != nil {
 		fmt.Println("Error displaying output, found error :", err)
@@ -218,6 +237,19 @@ Replica Count     :   {{.ReplicaCount}}
 		fmt.Println("Error displaying volume details, found error :", err)
 		return nil
 	}
+
+	tmpl, err = template.New("VolumeInfo").Parse(applicationInfoTemplate)
+	if err != nil {
+		fmt.Println("Error displaying output, found error :", err)
+		return nil
+	}
+
+	err = tmpl.Execute(os.Stdout, applicationInfo)
+	if err != nil {
+		fmt.Println("Error displaying volume details, found error :", err)
+		return nil
+	}
+
 	if v.GetCASType() == string(JivaStorageEngine) {
 		replicaCount, _ = strconv.Atoi(v.GetReplicaCount())
 		// This case will occur only if user has manually specified zero replica.
