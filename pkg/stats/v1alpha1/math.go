e7e23e59b64c3badcb5f8a0ab579d1201a65b36a
Utkarsh Mani Tripathi <utkarsh.tripathi@mayadata.io>
2019-01-31 09:53:32 +0530

    fix(exporter): fix linting error
    Signed-off-by: Utkarsh Mani Tripathi <utkarsh.tripathi@mayadata.io>

--- pkg/stats/v1alpha1/math.go
+++ pkg/stats/v1alpha1/math.go
@@ -2,7 +2,7 @@ package stats
 
 // DivideFloat64 returns the value of value divided by base.
 // divide by zero case handled before calling.
-func DivideFloat64(value float64, base float64) (float64, bool) {
+func DivideFloat64(value, base float64) (float64, bool) {
 	if base == 0 {
 		return 0, false
 	}
