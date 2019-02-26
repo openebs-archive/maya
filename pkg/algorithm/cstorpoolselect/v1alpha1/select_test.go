94892565a24135540f25b0b3288b8f7fa1744bd1
Ashish Ranjan <ashishranjan738@gmail.com>
2019-02-26 19:45:34 +0530

    feat(volume distribution, cstor replica, pool anti-affinity): distribute replicas across storagepools (#950)
    ###  Why we need this PR
    Users want to deploy applications using StatefulSet with application replication factor **"> 1"** and volume replication factor **"= 1"**; provided this application has the ability to replicate data across nodes in a given cluster. Some examples of such applications are _Cassandra_, _ElastiSearch_, _MongoDB_, etc. In other words, each instance of this kind of application(s) consume one volume and from the application p.o.v all these per-instance volumes should spread out across nodes to ensure High Availability of the application.
    
    ### What this PR does
    This commit adds the ability in cstor to correlate & hence distribute single replica volumes across pools _(which are in turn deployed in separate nodes)_ when application consuming all these volumes is deployed as a StatefulSet.
    
    #### Below are supported anti-affinity features:
    - `openebs.io/replica-anti-affinity: <unique_identification_of_app_in_cluster>`
    - `openebs.io/preferred-replica-anti-affinity: <unique_identification_of_app_in_cluster>`
    
    Below is an example of a statefulset.yaml that makes use of `openebs.io/replica-anti-affinity`:
    ```
    apiVersion: storage.k8s.io/v1
    kind: StorageClass
    metadata:
      annotations:
        cas.openebs.io/config: |
          - name: StoragePoolClaim
            value: "cstor-sparse-pool"
          - name: ReplicaCount
            value: "1"
        openebs.io/cas-type: cstor
      name: openebs-cstor-sparse-sts
    provisioner: openebs.io/provisioner-iscsi
    reclaimPolicy: Delete
    volumeBindingMode: Immediate
    ---
    apiVersion: v1
    kind: Service
    metadata:
      labels:
        app: busybox1
      name: busybox1
    spec:
      clusterIP: None
      selector:
        app: busybox1
    ---
    apiVersion: apps/v1
    kind: StatefulSet
    metadata:
      name: busybox1
      labels:
        app: busybox1
    spec:
      serviceName: busybox1
      replicas: 1
      selector:
        matchLabels:
          app: busybox1
          openebs.io/replica-anti-affinity: busybox1
      template:
        metadata:
          labels:
            app: busybox1
            openebs.io/replica-anti-affinity: busybox1
        spec:
          terminationGracePeriodSeconds: 1800
          containers:
          - name: busybox1
            image: ubuntu
            imagePullPolicy: IfNotPresent
            command:
              - sleep
              - infinity
            volumeMounts:
            - name: busybox1
              mountPath: /busybox1
      volumeClaimTemplates:
      - metadata:
          name: busybox1
        spec:
          accessModes: [ "ReadWriteOnce" ]
          storageClassName: openebs-cstor-sparse-sts
          resources:
            requests:
              storage: 1Gi
    ```
    
    fixes # https://github.com/openebs/openebs/issues/2389
    
    Signed-off-by: Ashish Ranjan <ashishranjan738@gmail.com>

---
+++ pkg/algorithm/cstorpoolselect/v1alpha1/select_test.go
@@ -0,0 +1,159 @@
+package v1alpha1
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPreferAntiAffinityLabel(t *testing.T) {
+	tests := map[string]struct {
+		label          string
+		expectedoutput policy
+	}{
+		"Mock Test 1": {"label1", preferAntiAffinityLabel{antiAffinityLabel{labelSelector: "label1"}}},
+		"Mock Test 2": {"label2", preferAntiAffinityLabel{antiAffinityLabel{labelSelector: "label2"}}},
+		"Mock Test 3": {"label3", preferAntiAffinityLabel{antiAffinityLabel{labelSelector: "label3"}}},
+		"Mock Test 4": {"label4", preferAntiAffinityLabel{antiAffinityLabel{labelSelector: "label4"}}},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			bo := PreferAntiAffinityLabel(test.label)
+			s := &selection{}
+			bo(s)
+			if !reflect.DeepEqual(s.policies[len(s.policies)-1], test.expectedoutput) {
+				t.Fatalf("test %q failed : expected %v but got %v", name, test.expectedoutput, s.policies[len(s.policies)-1])
+			}
+		})
+	}
+}
+
+func TestAntiAffinityLabel(t *testing.T) {
+	tests := map[string]struct {
+		mocklabel      string
+		expectedoutput policy
+	}{
+		"Mock Test 1": {"label1", antiAffinityLabel{labelSelector: "label1"}},
+		"Mock Test 2": {"label2", antiAffinityLabel{labelSelector: "label2"}},
+		"Mock Test 3": {"label3", antiAffinityLabel{labelSelector: "label3"}},
+		"Mock Test 4": {"label4", antiAffinityLabel{labelSelector: "label4"}},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			bo := AntiAffinityLabel(test.mocklabel)
+			s := &selection{}
+			bo(s)
+			if !reflect.DeepEqual(s.policies[len(s.policies)-1], test.expectedoutput) {
+				t.Fatalf("test %q failed : expected %v but got %v", name, test.expectedoutput, s.policies[len(s.policies)-1])
+			}
+		})
+	}
+}
+
+func TestValidate(t *testing.T) {
+	tests := map[string]struct {
+		isPreferAntiAffinity, isAntiAffinity bool
+		expectedError                        bool
+	}{
+		"Mock Test 1": {false, false, false},
+		"Mock Test 2": {true, false, false},
+		"Mock Test 3": {false, true, false},
+		"Mock Test 4": {true, true, true},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			mockSelection := &selection{}
+			if test.isAntiAffinity {
+				p := AntiAffinityLabel("antiAffinity")
+				p(mockSelection)
+			}
+
+			if test.isPreferAntiAffinity {
+				p := PreferAntiAffinityLabel("preferedAntiAffinity")
+				p(mockSelection)
+			}
+
+			err := mockSelection.validate()
+			if test.expectedError && err == nil {
+				t.Fatalf("Test %q failed: expected error not to be nil", name)
+			}
+			if !test.expectedError && err != nil {
+				t.Fatalf("Test %q failed: expected error to be nil", name)
+			}
+		})
+	}
+}
+
+func TestTemplateFunctions(t *testing.T) {
+	tests := map[string]struct {
+		expectedLength int
+	}{
+		"Test 1": {3},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			p := TemplateFunctions()
+			if len(p) != test.expectedLength {
+				t.Fatalf("test %q failed: expected items %v but got %v", name, test.expectedLength, len(p))
+			}
+		})
+	}
+}
+
+func TestName(t *testing.T) {
+	tests := map[string]struct {
+		Invoker      policy
+		expectedName string
+	}{
+		"Test 1": {antiAffinityLabel{}, "anti-affinity-label"},
+		"Test 2": {preferAntiAffinityLabel{}, "prefer-anti-affinity-label"},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			n := test.Invoker.name()
+			if !reflect.DeepEqual(string(n), test.expectedName) {
+				t.Fatalf("test %q failed : expected %v but got %v", name, test.expectedName, string(n))
+			}
+		})
+	}
+}
+
+func TestNewSelection(t *testing.T) {
+	tests := map[string]struct {
+		expectedUIDs, expectedBuildOptions                 int
+		UIDs, preferAntiAffinityLabels, AntiAffinityLabels []string
+	}{
+		"Test 1": {1, 2, []string{"uid1"}, []string{"PAlabel1"}, []string{"Alabel1"}},
+		"Test 2": {2, 3, []string{"uid1", "uid2"}, []string{"PAlabel1", "PAlabel2"}, []string{"Alabel1"}},
+		"Test 3": {3, 3, []string{"uid1", "uid2", "uid3"}, []string{"PAlabel1"}, []string{"Alabel1", "Alabel2"}},
+		"Test 4": {4, 4, []string{"uid1", "uid2", "uid3", "uid4"}, []string{"PAlabel1", "PAlabel2", "PAlabel3"}, []string{"Alabel1"}},
+		"Test 5": {5, 4, []string{"uid1", "uid2", "uid3", "uid4", "uid5"}, []string{"PAlabel1"}, []string{"Alabel1", "Alabel2", "Alabel3"}},
+		"Test 6": {6, 5, []string{"uid1", "uid2", "uid3", "uid4", "uid5", "uid6"}, []string{"PAlabel1", "PAlabel2", "PAlabel3", "PAlabel4"}, []string{"Alabel1"}},
+		"Test 7": {7, 5, []string{"uid1", "uid2", "uid3", "uid4", "uid5", "uid6", "uid7"}, []string{"PAlabel1"}, []string{"Alabel1", "Alabel2", "Alabel3", "Alabel4"}},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			mockBuildOptions := []buildOption{}
+			for _, lab := range test.AntiAffinityLabels {
+				mockBuildOptions = append(mockBuildOptions, AntiAffinityLabel(lab))
+			}
+
+			for _, lab := range test.preferAntiAffinityLabels {
+				mockBuildOptions = append(mockBuildOptions, PreferAntiAffinityLabel(lab))
+			}
+
+			p := newSelection(test.UIDs, mockBuildOptions...)
+			if len(p.policies) != test.expectedBuildOptions {
+				t.Fatalf("test %q failed: expected %v but got %v", name, test.expectedBuildOptions, len(p.policies))
+			}
+			if len(p.poolUIDs) != test.expectedUIDs {
+				t.Fatalf("test %q failed: expected %v but got %v", name, test.expectedUIDs, p.poolUIDs)
+			}
+		})
+	}
+}
---
+++ pkg/cstorvolumereplica/v1alpha1/kubernetes.go
@@ -0,0 +1,96 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	apis "github.com/openebs/maya/pkg/apis/openebs.io/v1alpha1"
+	clientset "github.com/openebs/maya/pkg/client/generated/clientset/internalclientset"
+	kclient "github.com/openebs/maya/pkg/client/k8s/v1alpha1"
+)
+
+// getClientsetFn is a typed function that
+// abstracts fetching of internal clientset
+type getClientsetFn func() (clientset *clientset.Clientset, err error)
+
+// listFn is a typed function that abstracts
+// listing of cstor volume replica instances
+type listFn func(cli *clientset.Clientset, namespace string, opts metav1.ListOptions) (*apis.CStorVolumeReplicaList, error)
+
+// kubeclient enables kubernetes API operations
+// on cstor volume replica instance
+type kubeclient struct {
+	// clientset refers to cstor volume replica's
+	// clientset that will be responsible to
+	// make kubernetes API calls
+	clientset *clientset.Clientset
+
+	// functions useful during mocking
+	getClientset getClientsetFn
+	list         listFn
+}
+
+// kubeclientBuildOption defines the abstraction
+// to build a kubeclient instance
+type kubeclientBuildOption func(*kubeclient)
+
+// withDefaults sets the default options
+// of kubeclient instance
+func (k *kubeclient) withDefaults() {
+	if k.getClientset == nil {
+		k.getClientset = func() (clients *clientset.Clientset, err error) {
+			config, err := kclient.Config().Get()
+			if err != nil {
+				return nil, err
+			}
+			return clientset.NewForConfig(config)
+		}
+	}
+	if k.list == nil {
+		k.list = func(cli *clientset.Clientset, namespace string, opts metav1.ListOptions) (*apis.CStorVolumeReplicaList, error) {
+			return cli.OpenebsV1alpha1().CStorVolumeReplicas(namespace).List(opts)
+		}
+	}
+}
+
+// WithKubeClient sets the kubernetes client against
+// the kubeclient instance
+func WithKubeClient(c *clientset.Clientset) kubeclientBuildOption {
+	return func(k *kubeclient) {
+		k.clientset = c
+	}
+}
+
+// KubeClient returns a new instance of kubeclient meant for
+// cstor volume replica operations
+func KubeClient(opts ...kubeclientBuildOption) *kubeclient {
+	k := &kubeclient{}
+	for _, o := range opts {
+		o(k)
+	}
+	k.withDefaults()
+	return k
+}
+
+// getClientOrCached returns either a new instance
+// of kubernetes client or its cached copy
+func (k *kubeclient) getClientOrCached() (*clientset.Clientset, error) {
+	if k.clientset != nil {
+		return k.clientset, nil
+	}
+	c, err := k.getClientset()
+	if err != nil {
+		return nil, err
+	}
+	k.clientset = c
+	return k.clientset, nil
+}
+
+// List returns a list of cstor volume replica
+// instances present in kubernetes cluster
+func (k *kubeclient) List(namespace string, opts metav1.ListOptions) (*apis.CStorVolumeReplicaList, error) {
+	cli, err := k.getClientOrCached()
+	if err != nil {
+		return nil, err
+	}
+	return k.list(cli, namespace, opts)
+}
