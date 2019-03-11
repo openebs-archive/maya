3fbe26ceb17c097b62226f95c3482a6df1aa4df0
Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>
2019-03-11 18:55:03 +0530

    [TA5006] feat(exporter): add pool exporter yaml in runTask (#960)
    * [TA5006] feat(exporter): add pool exporter yaml in runTask
    
    ### What this PR does / why we need it:
    Add pool exporter yaml in existing cstor-pool-create-putcstorpooldeployment-default runTask. Pool exporter will be used as sidecar.
    
    ### Additional Info for Reviewer:
    Following env should be added into openebs-operator.yaml which will be used by m-apiserver for deployment purpose.
    
    ```yaml
     - name: OPENEBS_IO_CSTOR_POOL_EXPORTER_IMAGE                                                                                                               
       value: "utkarshmani1997/m-exporter:pool-exporter7"
    ```
    
    Helm chart need to be updated accordingly.
    
    Signed-off-by: Utkarsh Mani Tripathi <utkarsh.tripathi@mayadata.io>

--- pkg/install/v1alpha1/cstor_pool.go
+++ pkg/install/v1alpha1/cstor_pool.go
@@ -31,6 +31,10 @@ spec:
   # communicates with cstor iscsi target
   - name: CstorPoolImage
     value: {{env "OPENEBS_IO_CSTOR_POOL_IMAGE" | default "openebs/cstor-pool:latest"}}
+  # CstorPoolExporterImage is the container image that executes zpool and zfs binary
+  # to export various volume and pool metrics
+  - name: CstorPoolExporterImage
+    value: {{env "OPENEBS_IO_CSTOR_POOL_EXPORTER_IMAGE" | default "openebs/m-exporter:latest"}}
   # CstorPoolMgmtImage runs cstor pool and cstor volume replica related CRUD
   # operations
   - name: CstorPoolMgmtImage
@@ -53,7 +57,7 @@ spec:
     value: {{env "OPENEBS_SERVICE_ACCOUNT"}}
   # PoolResourceRequests allow you to specify resource requests that need to be available
   # before scheduling the containers. If not specified, the default is to use the limits
-  # from PoolResourceLimits or the default requests set in the cluster. 
+  # from PoolResourceLimits or the default requests set in the cluster.
   - name: PoolResourceRequests
     value: "none"
   # PoolResourceLimits allow you to set the limits on memory and cpu for pool pods
@@ -170,7 +174,7 @@ spec:
     {{- $auxResourceRequestsVal := fromYaml .Config.AuxResourceRequests.value -}}
     {{- $setAuxResourceLimits := .Config.AuxResourceLimits.value | default "none" -}}
     {{- $auxResourceLimitsVal := fromYaml .Config.AuxResourceLimits.value -}}
-    apiVersion: extensions/v1beta1
+    apiVersion: apps/v1beta1
     kind: Deployment
     metadata:
       name: {{.TaskResult.putcstorpoolcr.objectName}}
@@ -180,13 +184,15 @@ spec:
         app: cstor-pool
         openebs.io/version: {{ .CAST.version }}
         openebs.io/cas-template-name: {{ .CAST.castName }}
+      annotations:
+        openebs.io/monitoring: pool_exporter_prometheus
       ownerReferences:
       - apiVersion: openebs.io/v1alpha1
         blockOwnerDeletion: true
         controller: true
         kind: CStorPool
         name: {{ .TaskResult.putcstorpoolcr.objectName }}
-        uid: {{ .TaskResult.putcstorpoolcr.objectUID }}    
+        uid: {{ .TaskResult.putcstorpoolcr.objectUID }}
     spec:
       strategy:
         type: Recreate
@@ -198,6 +204,11 @@ spec:
         metadata:
           labels:
             app: cstor-pool
+          annotations:
+            openebs.io/monitoring: pool_exporter_prometheus
+            prometheus.io/path: /metrics
+            prometheus.io/port: "9500"
+            prometheus.io/scrape: "true"
             openebs.io/storage-pool-claim: {{.Storagepool.owner}}
         spec:
           serviceAccountName: {{ .Config.ServiceAccountName.value }}
@@ -272,9 +283,6 @@ spec:
                 {{ $rKey }}: {{ $rLimit }}
               {{- end }}
               {{- end }}
-            ports:
-            - containerPort: 9500
-              protocol: TCP
             securityContext:
               privileged: true
             volumeMounts:
@@ -300,6 +308,37 @@ spec:
                   fieldPath: metadata.namespace
             - name: RESYNC_INTERVAL
               value: {{ .Config.ResyncInterval.value }}
+          - name: maya-exporter
+            image: {{ .Config.CstorPoolExporterImage.value }}
+            resources:
+              {{- if ne $setAuxResourceRequests "none" }}
+              requests:
+              {{- range $rKey, $rLimit := $auxResourceRequestsVal }}
+                {{ $rKey }}: {{ $rLimit }}
+              {{- end }}
+              {{- end }}
+              {{- if ne $setAuxResourceLimits "none" }}
+              limits:
+              {{- range $rKey, $rLimit := $auxResourceLimitsVal }}
+                {{ $rKey }}: {{ $rLimit }}
+              {{- end }}
+              {{- end }}
+            command:
+            - maya-exporter
+            args:
+            - "-e=pool"
+            ports:
+            - containerPort: 9500
+              protocol: TCP
+            volumeMounts:
+            - mountPath: /dev
+              name: device
+            - mountPath: /tmp
+              name: tmp
+            - mountPath: {{ .Config.SparseDir.value }}
+              name: sparse
+            - mountPath: /run/udev
+              name: udev
           volumes:
           - name: device
             hostPath:
