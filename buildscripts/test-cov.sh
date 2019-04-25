5e977885a9e10e6b21bdc1f66c0fc9dbe3bf7bbd
mittachaitu <sai.chaithanya@mayadata.io>
2019-04-25 13:48:35 +0530

    Include integration-test package in travis unit test
    Signed-off-by: mittachaitu <sai.chaithanya@mayadata.io>

--- buildscripts/test-cov.sh
+++ buildscripts/test-cov.sh
@@ -3,9 +3,9 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v 'vendor\|pkg/apis\|pkg/client/generated\|integration-tests'); do
+for d in $(go list ./... | grep -v 'vendor\|pkg/apis\|pkg/client/generated'); do
     #TODO - Include -race while creating the coverage profile.
-    go test -coverprofile=profile.out -covermode=atomic $d
+    go test -c -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
