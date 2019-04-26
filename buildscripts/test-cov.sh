1acb361d3ae149e4c8fb70473b6a8d5e6a8b54e3
mittachaitu <sai.chaithanya@mayadata.io>
2019-04-26 13:07:10 +0530

    This commit adds vet test for integration-test package
    Signed-off-by: mittachaitu <sai.chaithanya@mayadata.io>

--- buildscripts/test-cov.sh
+++ buildscripts/test-cov.sh
@@ -3,11 +3,17 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v 'vendor\|pkg/apis\|pkg/client/generated'); do
+for d in $(go list ./... | grep -v 'vendor\|pkg/apis\|pkg/client/generated\|integration-tests'); do
     #TODO - Include -race while creating the coverage profile.
-    go test -c -coverprofile=profile.out -covermode=atomic $d
+    go test -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
+done
+
+echo "Running go vet on integration-test"
+for d in $(go list ./... | grep 'integration-tests'); do
+    #TODO - Currently we are checking only on integration-test.
+    go vet $d
 done
