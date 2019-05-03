64e196b625dfb8abaa0add4a5340576b7bd4f16a
mayank <mayank.patel@cloudbyte.com>
2019-05-03 11:43:30 +0530

    Fixing travis build
    Signed-off-by: mayank <mayank.patel@cloudbyte.com>

--- pkg/client/generated/clientset/versioned/typed/openebs.io/v1alpha1/cstorrestore.go
+++ pkg/client/generated/clientset/versioned/typed/openebs.io/v1alpha1/cstorrestore.go
@@ -20,7 +20,7 @@ package v1alpha1
 
 import (
 	v1alpha1 "github.com/openebs/maya/pkg/apis/openebs.io/v1alpha1"
-	scheme "github.com/openebs/maya/pkg/client/generated/clientset/internalclientset/scheme"
+	scheme "github.com/openebs/maya/pkg/client/generated/clientset/versioned/scheme"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
