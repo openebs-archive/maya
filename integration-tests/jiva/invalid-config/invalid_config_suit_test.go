58de0e36b6059db26082208744a95b8d6d957c68
mittachaitu <sai.chaithanya@mayadata.io>
2019-05-07 23:15:35 +0530

    This commit adds the validation test in builder pattern
    Signed-off-by: mittachaitu <sai.chaithanya@mayadata.io>

--- integration-tests/jiva/invalid-config/invalid_config_suit_test.go
+++ integration-tests/jiva/invalid-config/invalid_config_suit_test.go
@@ -1,63 +1,55 @@
+/*
+Copyright 2019 The OpenEBS Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package invalidconfig
 
 import (
 	"flag"
-	"os"
+	"strconv"
+	"time"
 
 	"testing"
-
-	"github.com/openebs/maya/pkg/client/k8s/v1alpha1"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/openebs/maya/integration-tests/artifacts"
 	installer "github.com/openebs/maya/integration-tests/artifacts/installer/v1alpha1"
-	"github.com/openebs/maya/integration-tests/kubernetes"
+	ns "github.com/openebs/maya/pkg/kubernetes/namespace/v1alpha1"
 	node "github.com/openebs/maya/pkg/kubernetes/node/v1alpha1"
+	pvc "github.com/openebs/maya/pkg/kubernetes/persistentvolumeclaim/v1alpha1"
 	pod "github.com/openebs/maya/pkg/kubernetes/pod/v1alpha1"
+	sc "github.com/openebs/maya/pkg/kubernetes/storageclass/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	kube "k8s.io/client-go/kubernetes"
 
 	// auth plugins
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
 const (
-	// defaultTimeOut is the default time in seconds
-	// for Eventually block
-	defaultTimeOut int = 500
-	// defaultPollingInterval is the default polling
-	// time in seconds for the Eventually block
-	defaultPollingInterval int = 10
-	// minNodeCount is the minimum number of nodes
-	// need to run this test
-	minNodeCount      int                      = 3
-	parentDir         artifacts.ArtifactSource = "../"
-	namespaceArtifact artifacts.ArtifactSource = "namespace_validation.yaml"
+	MaxRetry                              = 30
+	minNodeCount int                      = 1
+	parentDir    artifacts.ArtifactSource = "../"
 )
 
-func isPodRunning(namespace, lselector string, podCount int) {
-	// Verify phase of the pod
-	Eventually(func() int {
-		pods, err := pod.
-			KubeClient(pod.WithNamespace(namespace)).
-			List(metav1.ListOptions{LabelSelector: lselector})
-		Expect(err).ShouldNot(HaveOccurred())
-		return pod.
-			ListBuilder().
-			WithAPIList(pods).
-			WithFilter(pod.IsRunning()).
-			List().
-			Len()
-	},
-		defaultTimeOut, defaultPollingInterval).
-		Should(Equal(podCount), "Pod count should be "+string(podCount))
+type clientOps struct {
+	podClient *pod.KubeClient
+	scClient  *sc.Kubeclient
+	pvcClient *pvc.Kubeclient
+	nsClient  *ns.Kubeclient
 }
-
-var (
-	cl                    *kube.Clientset
-	defaultoebsComponents []*installer.DefaultInstaller
-)
 
 func TestSource(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -65,30 +57,24 @@ func TestSource(t *testing.T) {
 }
 
 func init() {
-	flag.Parse()
+	flag.StringVar(&kubeConfigPath, "kubeConfigPath", "", "Based on arguments test will be triggered on corresponding cluster")
 }
 
 var _ = BeforeSuite(func() {
 	var errs []error
-	// Fetching the kube config path
-	configPath, err := kubernetes.GetConfigPath()
-	Expect(err).ShouldNot(HaveOccurred())
-
-	// Setting the path in environemnt variable
-	err = os.Setenv(string(v1alpha1.KubeConfigEnvironmentKey), configPath)
-	Expect(err).ShouldNot(HaveOccurred())
 
 	// Check the running node count
-	nodes, err := node.
-		KubeClient().
-		List(metav1.ListOptions{})
+	nodesClient := node.
+		NewKubeClient(node.WithKubeConfigPath(kubeConfigPath))
+	nodes, err := nodesClient.List(metav1.ListOptions{})
+	Expect(err).ShouldNot(HaveOccurred())
 	nodeCnt := node.
 		NewListBuilder().
 		WithAPIList(nodes).
 		WithFilter(node.IsReady()).
 		List().
 		Len()
-	Expect(nodeCnt).Should(Equal(minNodeCount), "Running node count should be "+string(nodeCnt))
+	Expect(nodeCnt).Should(Equal(minNodeCount), "Running node count should be "+strconv.Itoa(int(minNodeCount)))
 
 	// Fetch openebs component artifacts
 	artifactsOpenEBS, errs := artifacts.GetArtifactsListUnstructuredFromFile(parentDir + artifacts.OpenEBSArtifacts)
@@ -105,31 +91,32 @@ var _ = BeforeSuite(func() {
 		defaultoebsComponents = append(defaultoebsComponents, oebsComponentInstaller)
 	}
 
-	testNamespaceArtifact, err := artifacts.GetArtifactUnstructuredFromFile(namespaceArtifact)
-	Expect(err).ShouldNot(HaveOccurred())
-	namespaceInstaller, err := installer.BuilderForObject(testNamespaceArtifact).Build()
-	Expect(err).ShouldNot(HaveOccurred())
-	err = namespaceInstaller.Install()
-	Expect(err).ShouldNot(HaveOccurred())
-	defaultoebsComponents = append(defaultoebsComponents, namespaceInstaller)
+	// get pod client set
+	cOps = cOps.newPodClient("openebs")
 
 	// Check for maya-apiserver pod to get created and running
-	isPodRunning(string(artifacts.OpenebsNamespace), string(artifacts.MayaAPIServerLabelSelector), 1)
+	podCount := cOps.isRunningPodCount(string(artifacts.OpenebsNamespace), string(artifacts.MayaAPIServerLabelSelector), 1)
+	Expect(podCount).To(Equal(1))
 
 	// Check for provisioner pod to get created and running
-	isPodRunning(string(artifacts.OpenebsNamespace), string(artifacts.OpenEBSProvisionerLabelSelector), 1)
+	podCount = cOps.isRunningPodCount(string(artifacts.OpenebsNamespace), string(artifacts.OpenEBSProvisionerLabelSelector), 1)
+	Expect(podCount).To(Equal(1))
 
 	// Check for snapshot operator to get created and running
-	isPodRunning(string(artifacts.OpenebsNamespace), string(artifacts.OpenEBSSnapshotOperatorLabelSelector), 1)
+	podCount = cOps.isRunningPodCount(string(artifacts.OpenebsNamespace), string(artifacts.OpenEBSSnapshotOperatorLabelSelector), 1)
+	Expect(podCount).To(Equal(1))
 
 	// Check for admission server to get created and running
-	isPodRunning(string(artifacts.OpenebsNamespace), string(artifacts.OpenEBSAdmissionServerLabelSelector), 1)
+	podCount = cOps.isRunningPodCount(string(artifacts.OpenebsNamespace), string(artifacts.OpenEBSAdmissionServerLabelSelector), 1)
+	Expect(podCount).To(Equal(1))
 
 	// Check for NDM pods to get created and running
-	isPodRunning(string(artifacts.OpenebsNamespace), string(artifacts.OpenEBSNDMLabelSelector), minNodeCount)
+	podCount = cOps.isRunningPodCount(string(artifacts.OpenebsNamespace), string(artifacts.OpenEBSNDMLabelSelector), minNodeCount)
+	Expect(podCount).To(Equal(minNodeCount))
 
 	// Check for cstor storage pool pods to get created and running
-	isPodRunning(string(artifacts.OpenebsNamespace), string(artifacts.OpenEBSCStorPoolLabelSelector), minNodeCount)
+	_ = cOps.isRunningPodCount(string(artifacts.OpenebsNamespace), string(artifacts.OpenEBSCStorPoolLabelSelector), 1)
+	Expect(podCount).To(Equal(1))
 
 	By("OpenEBS components are in running state")
 })
@@ -141,3 +128,62 @@ var _ = AfterSuite(func() {
 		Expect(err).ShouldNot(HaveOccurred())
 	}
 })
+
+var cOps = &clientOps{}
+
+var (
+	defaultoebsComponents []*installer.DefaultInstaller
+	kubeConfigPath        string
+)
+
+func (cOps *clientOps) newPodClient(namespace string) *clientOps {
+	newPodClient := pod.NewKubeClient(pod.WithKubeConfigPath(kubeConfigPath), pod.WithNamespace(namespace))
+	cOps.podClient = newPodClient
+	return cOps
+}
+
+func (cOps *clientOps) newNsClient() *clientOps {
+	newNsClient := ns.NewKubeClient(ns.WithKubeConfigPath(kubeConfigPath))
+	cOps.nsClient = newNsClient
+	return cOps
+}
+
+func (cOps *clientOps) newSCClient() *clientOps {
+	newSCClient := sc.NewKubeClient(sc.WithKubeConfigPath(kubeConfigPath))
+	cOps.scClient = newSCClient
+	return cOps
+}
+
+func (cOps *clientOps) newPVCClient(namespace string) *clientOps {
+	newPVCClient := pvc.NewKubeClient(pvc.WithKubeConfigPath(kubeConfigPath), pvc.WithNamespace(namespace))
+	cOps.pvcClient = newPVCClient
+	return cOps
+}
+
+func (cOps *clientOps) isRunningPodCount(namespace, lselector string, expectedPodCount int) int {
+	var maxRetry int
+	var podCount int
+	maxRetry = MaxRetry
+	for i := 0; i < maxRetry; i++ {
+		podCount = cOps.getRunningPodCount(namespace, lselector)
+		if podCount == expectedPodCount {
+			return podCount
+		}
+		if maxRetry == 0 {
+			break
+		}
+		time.Sleep(5 * time.Second)
+	}
+	return podCount
+}
+
+func (cOps *clientOps) getRunningPodCount(namespace, lselector string) int {
+	pods, err := cOps.podClient.
+		List(metav1.ListOptions{LabelSelector: lselector})
+	Expect(err).ShouldNot(HaveOccurred())
+	return pod.
+		ListBuilderForAPIList(pods).
+		WithFilter(pod.IsRunning()).
+		List().
+		Len()
+}
--- integration-tests/jiva/invalid-config/invalid_config_test.go
+++ integration-tests/jiva/invalid-config/invalid_config_test.go
@@ -1,58 +1,221 @@
+/*
+Copyright 2019 The OpenEBS Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package invalidconfig
 
+// TODO: Used third party library to get maya-apiserver logs(change upon review
+// comments)
+
 import (
+	"fmt"
+
+	script "github.com/bitfield/script"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/openebs/maya/integration-tests/artifacts"
+	apis "github.com/openebs/maya/pkg/apis/openebs.io/v1alpha1"
+	ns "github.com/openebs/maya/pkg/kubernetes/namespace/v1alpha1"
+	pvc "github.com/openebs/maya/pkg/kubernetes/persistentvolumeclaim/v1alpha1"
+	pod "github.com/openebs/maya/pkg/kubernetes/pod/v1alpha1"
+	sc "github.com/openebs/maya/pkg/kubernetes/storageclass/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var (
-	scArtifacts = map[string]artifacts.ArtifactSource{
-		"bad_sc_with_semicolumn": artifacts.ArtifactSource("resources_with_bad_sc.yaml"),
-	}
-	pvcArtifacts = map[string]artifacts.ArtifactSource{
-		"bad_pvc_with_semicolumn": artifacts.ArtifactSource("resources_with_bad_pvc.yaml"),
-	}
-	pvcsInstaller, scsInstaller []*TestInstaller
+	// defaultReplicaLabel represents the jiva replica
+	defaultReplicaLabel = "openebs.io/replica=jiva-replica"
+	// defaultCtrlLabel represents the jiva controller
+	defaultCtrlLabel      = "openebs.io/controller=jiva-controller"
+	openebsProvisioner    = "openebs.io/provisioner-iscsi"
+	accessModes           = []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce}
+	capacity              = "5G"
+	scObj                 *storagev1.StorageClass
+	pvcObj                *corev1.PersistentVolumeClaim
+	nsObj                 *corev1.Namespace
+	openebsCASConfigValue = "- name: ReplicaCount:\n  Value: 1"
+	infraNamespace        = "openebs"
 )
 
-var _ = Describe("Debuging proper error logs", func() {
+var _ = Describe("TEST INVALID CONFIGURATIONS IN SC", func() {
+	var (
+		namespaceName = "validation-ns1"
+		scName        = "jiva-invalid-config-sc"
+		pvcName       = "jiva-volume-claim"
+		//errMsg        = "invalid sc cas config: " + openebsCASConfigValue + ": error converting YAML to JSON:"
+		errMsg = "invalid sc cas config: - name: ReplicaCount:"
+	)
 	BeforeEach(func() {
-		By("Deploy required storageclasses")
-		for _, artifact := range scArtifacts {
-			installerObj := NewTestInstaller().
-				WithArtifact(artifact).
-				GetUnstructObj().
-				GetInstallerObj().
-				Install()
-			isPresent := installerObj.isSCDeployed()
-			Expect(isPresent).Should(BeTrue())
-			scsInstaller = append(scsInstaller, installerObj)
+		annotations := map[string]string{
+			string(apis.CASTypeKey):   string(apis.JivaVolume),
+			string(apis.CASConfigKey): openebsCASConfigValue,
 		}
+		var err error
+		cOps = cOps.newPodClient(namespaceName).
+			newSCClient().
+			newPVCClient(namespaceName).
+			newNsClient()
+
+		nsObj, err = ns.NewBuilder().
+			WithName(namespaceName).
+			Build()
+		Expect(err).ShouldNot(HaveOccurred())
+
+		scObj, err = sc.NewBuilder().
+			WithName(scName).
+			WithAnnotations(annotations).
+			WithProvisioner(openebsProvisioner).Build()
+		Expect(err).ShouldNot(HaveOccurred())
+
+		pvcObj, err = pvc.NewBuilder().
+			WithName(pvcName).
+			WithNamespace(namespaceName).
+			WithStorageClass(scName).
+			WithAccessModes(accessModes).
+			WithCapacity(capacity).Build()
+		Expect(err).ShouldNot(HaveOccurred())
 	})
-	AfterEach(func() {
-		for _, installer := range scsInstaller {
-			err := installer.ComponentInstaller.UnInstall()
+
+	When("We apply valid pvc and invalid sc yaml in k8s cluster", func() {
+		It("Maya-apiserver log should show error message related to invalid configurations", func() {
+			By(fmt.Sprintf("Create test specific namespace {%s}", namespaceName))
+			_, err := cOps.nsClient.Create(nsObj)
+			Expect(err).To(BeNil())
+
+			By(fmt.Sprintf("Create storageclass named {%s}", scName))
+			_, err = cOps.scClient.Create(scObj)
+			Expect(err).To(BeNil())
+
+			By(fmt.Sprintf("Create PVC named {%s} in Namespace: {%s}", pvcName, namespaceName))
+			_, err = cOps.pvcClient.Create(pvcObj)
+			Expect(err).To(BeNil())
+
+			pvcLabel := string(apis.PersistentVolumeClaimKey) + "=" + pvcName
+			replicaLabel := defaultReplicaLabel + "," + pvcLabel
+			ctrlLabel := defaultCtrlLabel + "," + pvcLabel
+
+			By("jiva-ctrl pod should not come to running state")
+			// Check jiva controller pod should not create
+			podCount := cOps.isRunningPodCount(namespaceName, ctrlLabel, 1)
+			Expect(podCount).To(Equal(0))
+
+			By("jiva-replica pod should not come to running state")
+			// Check jiva controller pod should not create
+			podCount = cOps.isRunningPodCount(namespaceName, replicaLabel, 3)
+			Expect(podCount).To(Equal(0))
+
+			// TODO: used pipe to get the logs of maya-apiserver and verify
+			// the error logs are showing about invalid configurations(but
+			// change based on review comments)
+			// TODO: Refactor below snippet in builder pattern
+			openebsPodKubeClient := pod.NewKubeClient(pod.WithNamespace(infraNamespace))
+			pods, err := openebsPodKubeClient.
+				List(metav1.ListOptions{LabelSelector: string(artifacts.MayaAPIServerLabelSelector)})
+			podName := pods.Items[0].ObjectMeta.Name
+			cmd := "kubectl logs -n " + infraNamespace + " " + podName
+			lines, err := script.Exec(cmd).Match(errMsg).CountLines()
 			Expect(err).ShouldNot(HaveOccurred())
-		}
-	})
-
-	Context("test debug logs in maya-apiserver", func() {
-		It("should show perfect logs regarding error", func() {
-			By("Deploy persistent volume claim")
-			for _, artifact := range pvcArtifacts {
-				installerObj := NewTestInstaller().
-					WithArtifact(artifact).
-					GetUnstructObj().
-					GetInstallerObj().
-					Install()
-				pvcsInstaller = append(pvcsInstaller, installerObj)
-			}
-
-			for _, installer := range pvcsInstaller {
-				err := installer.ComponentInstaller.UnInstall()
-				Expect(err).ShouldNot(HaveOccurred())
-			}
+			Expect(lines).NotTo(Equal(0))
 		})
 	})
+
+	AfterEach(func() {
+		By(fmt.Sprintf("Delete PVC named {%s} in Namespace: {%s}", pvcName, namespaceName))
+		err := cOps.pvcClient.Delete(pvcName, &metav1.DeleteOptions{})
+		Expect(err).To(BeNil())
+
+		By(fmt.Sprintf("Delete storageclass named {%s}", scName))
+		err = cOps.scClient.Delete(scName, &metav1.DeleteOptions{})
+		Expect(err).To(BeNil())
+
+		By(fmt.Sprintf("Delete {%s} namespace", namespaceName))
+		err = cOps.nsClient.Delete(namespaceName, &metav1.DeleteOptions{})
+		Expect(err).To(BeNil())
+	})
+
+})
+
+var _ = Describe("TEST INVALID CONFIGURATIONS IN PVC", func() {
+	var (
+		namespaceName         = "validation-ns2"
+		scName                = "jiva-valid-config-sc"
+		pvcName               = "jiva-ivalid-config-volume-claim"
+		openebsCASConfigValue = "- name: ReplicaCount\n  Value: 1"
+		invalidPVCLabel       = map[string]string{"name": "jiva-ivalid-config-volume-claim:"}
+	)
+	BeforeEach(func() {
+		annotations := map[string]string{
+			string(apis.CASTypeKey):   string(apis.JivaVolume),
+			string(apis.CASConfigKey): openebsCASConfigValue,
+		}
+		var err error
+		cOps = cOps.newPodClient(namespaceName).
+			newSCClient().
+			newPVCClient(namespaceName).
+			newNsClient()
+
+		nsObj, err = ns.NewBuilder().
+			WithName(namespaceName).
+			Build()
+		Expect(err).ShouldNot(HaveOccurred())
+
+		scObj, err = sc.NewBuilder().
+			WithName(scName).
+			WithAnnotations(annotations).
+			WithProvisioner(openebsProvisioner).Build()
+		Expect(err).ShouldNot(HaveOccurred())
+
+		pvcObj, err = pvc.NewBuilder().
+			WithName(pvcName).
+			WithNamespace(namespaceName).
+			WithLabels(invalidPVCLabel).
+			WithStorageClass(scName).
+			WithAccessModes(accessModes).
+			WithCapacity(capacity).Build()
+		Expect(err).ShouldNot(HaveOccurred())
+	})
+
+	When("We apply valid sc and invalid pvc yaml in k8s cluster", func() {
+		It("PVC creation should give error because of invalid pvc yaml", func() {
+			By(fmt.Sprintf("Create test specific namespace {%s}", namespaceName))
+			_, err := cOps.nsClient.Create(nsObj)
+			Expect(err).To(BeNil())
+
+			By(fmt.Sprintf("Create storageclass named {%s}", scName))
+			_, err = cOps.scClient.Create(scObj)
+			Expect(err).To(BeNil())
+
+			By(fmt.Sprintf("Create PVC named {%s} in Namespace: {%s}", pvcName, namespaceName))
+			_, err = cOps.pvcClient.Create(pvcObj)
+			Expect(err).NotTo(BeNil())
+		})
+	})
+
+	AfterEach(func() {
+		By(fmt.Sprintf("Delete PVC named {%s} in Namespace: {%s}", pvcName, namespaceName))
+		err := cOps.pvcClient.Delete(pvcName, &metav1.DeleteOptions{})
+		Expect(err).NotTo(BeNil())
+
+		By(fmt.Sprintf("Delete storageclass named {%s}", scName))
+		err = cOps.scClient.Delete(scName, &metav1.DeleteOptions{})
+		Expect(err).To(BeNil())
+
+		err = cOps.nsClient.Delete(namespaceName, &metav1.DeleteOptions{})
+		Expect(err).To(BeNil())
+	})
+
 })
