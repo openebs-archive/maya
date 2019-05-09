a3540775a9494a668a76d59d37f4c26701f93812
kmova <kiran.mova@openebs.io>
2019-05-09 06:27:45 +0000

    feat(localpv): add localpv sanity test to travis ci
    Signed-off-by: kmova <kiran.mova@openebs.io>

--- ci/local_pv.sh
+++ ci/local_pv.sh
@@ -5,6 +5,7 @@ kubectl apply -f ${ARTIFACTS_DIR}/openebs-local-provisioner.yaml
 kubectl apply -f ${ARTIFACTS_DIR}/sc-hostpath.yaml
 sleep 30
 LOCALPV="INIT"
+kubectl apply -f ${ARTIFACTS_DIR}/hostpath-pv-pod.yaml
 for _ in $(seq 1 5) ; do
     phaseLocalPV=$(kubectl get pods busybox-hp --output="jsonpath={.status.phase}")
     if [ "$phaseLocalPV" == "Running" ]; then
@@ -13,7 +14,9 @@ for _ in $(seq 1 5) ; do
     else
         echo "busybox-hp is in:" $phaseLocalPV
         if [ "$phaseLocalPV" != "Running" ]; then
-            kubectl describe pods busybox-hp
+            	kubectl describe pods busybox-hp
+		kubectl get pvc
+		kubectl get pv
         fi
 	sleep 30
     fi
