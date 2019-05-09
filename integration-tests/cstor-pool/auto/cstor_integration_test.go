efed34c82c1f93285ad0a7f1882fdb98da7770fe
Ashutosh Kumar <ashutosh.kumar@openebs.io>
2019-05-10 02:28:49 +0530

    feat(BDD): Add bdd for CStorPoolCluster provisioning
    This BDD includes pool provisioning and reconciliation test for striped,
    mirrored, raidz, and raidz2 raid groups.
    This BDD also covers the aspect that for a given CStorPoolCluster no two cStor
    pool should be on the same node.
    
    Signed-off-by: Ashutosh Kumar <ashutosh.kumar@openebs.io>

--- integration-tests/cstor-pool/auto/cstor_integration_test.go
+++ integration-tests/cstor-pool/auto/cstor_integration_test.go
@@ -14,14 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package cstorpoolit
+package cstorpoolitauto
 
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	apis "github.com/openebs/maya/pkg/apis/openebs.io/v1alpha1"
 	csp "github.com/openebs/maya/pkg/cstorpool/v1alpha3"
-	spc "github.com/openebs/maya/pkg/storagepoolclaim/v1alpha1"
+	cspc "github.com/openebs/maya/pkg/cstorpoolcluster/v1alpha1"
 	"time"
 
 	//	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -37,8 +37,8 @@ const (
 
 // operations encapsulates various kubernetes object client set for operations.
 type operations struct {
-	cspClient *csp.Kubeclient
-	spcClient *spc.Kubeclient
+	cspClient  *csp.Kubeclient
+	cspcClient *cspc.Kubeclient
 }
 
 // TestIntegrationCstorPool function instantiate the cstor pool test suite.
@@ -49,13 +49,13 @@ func TestIntegrationCstorPool(t *testing.T) {
 	RunSpecs(t, "Cstor pool integration test suite")
 }
 
-var _ = Describe("STRIPED SPARSE SPC", func() {
+var _ = Describe("STRIPED SPARSE CSPC", func() {
 
 	var (
-		spcObj *apis.StoragePoolClaim
+		cspcObj *apis.CStorPoolCluster
 	)
 	BeforeEach(func() {
-		spcObj = spc.NewBuilder().
+		cspcObj = cspc.NewBuilder().
 			WithName("sparse-striped-auto").
 			WithDiskType(string(apis.TypeSparseCPV)).
 			WithMaxPool(3).
@@ -64,75 +64,89 @@ var _ = Describe("STRIPED SPARSE SPC", func() {
 			Build().Object
 	})
 
-	// Test Case #1 (sparse-striped-auto-spc). | TestType : Pool Creation
-	When("We apply sparse-striped-auto spc yaml with maxPool count equal to 3 on a k8s cluster having at least 3 capable node", func() {
+	// Test Case #1 (sparse-striped-auto-cspc). | TestType : Pool Creation
+	When("We apply sparse-striped-auto cspc yaml with maxPool count equal to 3 on a k8s cluster having at least 3 capable node", func() {
 		It("pool resources count should be 3 with no error and healthy status", func() {
-			// Create a storage pool claim
-			_, err := ops.spcClient.Create(spcObj)
+			// Create a cstor pool cluster
+			_, err := ops.cspcClient.Create(cspcObj)
 			Expect(err).To(BeNil())
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #2 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 1 pool out of 3 by deleting one of the csp", func() {
 		It("a new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 1)
+			ops.deleteCSP(cspcObj.Name, 1)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #3 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 2 pool out of 3 by deleting 2 csps", func() {
 		It("2 new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 2)
+			ops.deleteCSP(cspcObj.Name, 2)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #4 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 3 pool out of 3 by deleting 3 csps", func() {
 		It("3 new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 3)
+			ops.deleteCSP(cspcObj.Name, 3)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #5 : Dependent on above test case #1 . | TestType : Reconciliation
-	When("We update spc maxPool field to 1", func() {
+	When("We update cspc maxPool field to 1", func() {
 		It("only 1 cstor pool should be present ", func() {
-			// Get the latest spc
-			newSPC, err := ops.spcClient.Get(spcObj.Name, metav1.GetOptions{})
+			// Get the latest cspc
+			newCSPC, err := ops.cspcClient.Get(cspcObj.Name, metav1.GetOptions{})
 			Expect(err).To(BeNil())
 
-			// update the spc to set maxPool field to 1
-			obj := spc.BuilderForAPIObject(newSPC).WithMaxPool(1)
-			_, err = ops.spcClient.Update(obj.Spc.Object)
+			// update the cspc to set maxPool field to 1
+			obj := cspc.BuilderForAPIObject(newCSPC).WithMaxPool(1)
+			_, err = ops.cspcClient.Update(obj.CSPC.Object)
 			Expect(err).To(BeNil())
-
 
 			// We expect 1 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 1)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 1)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 })
 
-var _ = Describe("MIRRORED SPARSE SPC", func() {
+var _ = Describe("MIRRORED SPARSE CSPC", func() {
 
 	var (
-		spcObj *apis.StoragePoolClaim
+		cspcObj *apis.CStorPoolCluster
 	)
 	BeforeEach(func() {
-		spcObj = spc.NewBuilder().
+		cspcObj = cspc.NewBuilder().
 			WithName("sparse-mirrored-auto").
 			WithDiskType(string(apis.TypeSparseCPV)).
 			WithMaxPool(3).
@@ -141,56 +155,68 @@ var _ = Describe("MIRRORED SPARSE SPC", func() {
 			Build().Object
 	})
 
-	// Test Case #1 (sparse-striped-auto-spc). | TestType : Pool Creation
-	When("We apply sparse-mirrored-auto spc yaml with maxPool count equal to 3 on a k8s cluster having at least 3 capable node", func() {
+	// Test Case #1 (sparse-striped-auto-cspc). | TestType : Pool Creation
+	When("We apply sparse-mirrored-auto cspc yaml with maxPool count equal to 3 on a k8s cluster having at least 3 capable node", func() {
 		It("pool resources count should be 3 with no error and healthy status", func() {
-			// Create a storage pool claim
-			_, err := ops.spcClient.Create(spcObj)
+			// Create a cstor pool cluster
+			_, err := ops.cspcClient.Create(cspcObj)
 			Expect(err).To(BeNil())
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #2 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 1 pool out of 3 by deleting one of the csp", func() {
 		It("a new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 1)
+			ops.deleteCSP(cspcObj.Name, 1)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #3 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 2 pool out of 3 by deleting 2 csps", func() {
 		It("2 new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 2)
+			ops.deleteCSP(cspcObj.Name, 2)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #4 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 3 pool out of 3 by deleting 3 csps", func() {
 		It("3 new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 3)
+			ops.deleteCSP(cspcObj.Name, 3)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 })
 
-var _ = Describe("RAIDZ SPARSE SPC", func() {
+var _ = Describe("RAIDZ SPARSE CSPC", func() {
 
 	var (
-		spcObj *apis.StoragePoolClaim
+		cspcObj *apis.CStorPoolCluster
 	)
 	BeforeEach(func() {
-		spcObj = spc.NewBuilder().
+		cspcObj = cspc.NewBuilder().
 			WithName("sparse-raidz-auto").
 			WithDiskType(string(apis.TypeSparseCPV)).
 			WithMaxPool(3).
@@ -199,56 +225,68 @@ var _ = Describe("RAIDZ SPARSE SPC", func() {
 			Build().Object
 	})
 
-	// Test Case #1 (sparse-striped-auto-spc). | TestType : Pool Creation
-	When("We apply sparse-raidz-auto spc yaml with maxPool count equal to 3 on a k8s cluster having at least 3 capable node", func() {
+	// Test Case #1 (sparse-striped-auto-cspc). | TestType : Pool Creation
+	When("We apply sparse-raidz-auto cspc yaml with maxPool count equal to 3 on a k8s cluster having at least 3 capable node", func() {
 		It("pool resources count should be 3 with no error and healthy status", func() {
-			// Create a storage pool claim
-			_, err := ops.spcClient.Create(spcObj)
+			// Create a cstor pool cluster
+			_, err := ops.cspcClient.Create(cspcObj)
 			Expect(err).To(BeNil())
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #2 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 1 pool out of 3 by deleting one of the csp", func() {
 		It("a new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 1)
+			ops.deleteCSP(cspcObj.Name, 1)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #3 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 2 pool out of 3 by deleting 2 csps", func() {
 		It("2 new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 2)
+			ops.deleteCSP(cspcObj.Name, 2)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #4 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 3 pool out of 3 by deleting 3 csps", func() {
 		It("3 new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 3)
+			ops.deleteCSP(cspcObj.Name, 3)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 })
 
-var _ = Describe("RAIDZ2 SPARSE SPC", func() {
+var _ = Describe("RAIDZ2 SPARSE CSPC", func() {
 
 	var (
-		spcObj *apis.StoragePoolClaim
+		cspcObj *apis.CStorPoolCluster
 	)
 	BeforeEach(func() {
-		spcObj = spc.NewBuilder().
+		cspcObj = cspc.NewBuilder().
 			WithName("sparse-raidz2-auto").
 			WithDiskType(string(apis.TypeSparseCPV)).
 			WithMaxPool(3).
@@ -257,56 +295,67 @@ var _ = Describe("RAIDZ2 SPARSE SPC", func() {
 			Build().Object
 	})
 
-	// Test Case #1 (sparse-striped-auto-spc). | TestType : Pool Creation
-	When("We apply sparse-raidz2-auto spc yaml with maxPool count equal to 3 on a k8s cluster having at least 3 capable node", func() {
+	// Test Case #1 (sparse-striped-auto-cspc). | TestType : Pool Creation
+	When("We apply sparse-raidz2-auto cspc yaml with maxPool count equal to 3 on a k8s cluster having at least 3 capable node", func() {
 		It("pool resources count should be 3 with no error and healthy status", func() {
-			// Create a storage pool claim
-			_, err := ops.spcClient.Create(spcObj)
+			// Create a cstor pool cluster
+			_, err := ops.cspcClient.Create(cspcObj)
 			Expect(err).To(BeNil())
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #2 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 1 pool out of 3 by deleting one of the csp", func() {
 		It("a new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 1)
+			ops.deleteCSP(cspcObj.Name, 1)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #3 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 2 pool out of 3 by deleting 2 csps", func() {
 		It("2 new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 2)
+			ops.deleteCSP(cspcObj.Name, 2)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 	//Test Case #4 : Dependent on above test case #1 . | TestType : Reconciliation
 	When("We delete 3 pool out of 3 by deleting 3 csps", func() {
 		It("3 new pool should come up again", func() {
-			ops.deleteCSP(spcObj.Name, 3)
+			ops.deleteCSP(cspcObj.Name, 3)
 			// We expect 3 cstorPool objects.
-			cspCount := ops.isHealthyCspCount(spcObj.Name, 3)
+			cspCount := ops.isHealthyCspCount(cspcObj.Name, 3)
 			Expect(cspCount).To(Equal(3))
+
+			onDiffNode:=ops.isCSPOnDifferentNodes(cspcObj.Name)
+			Expect(onDiffNode).To(Equal(true))
 		})
 	})
 
 })
 
 var _ = AfterSuite(func() {
-	spcClient, err := spc.KubeClient().WithFlag(KubeConfigPath)
+	cspcClient := cspc.NewKubeClient(cspc.WithKubeConfigPath(KubeConfigPath))
+	cspcList, err := cspcClient.List(metav1.ListOptions{})
 	Expect(err).To(BeNil())
-	spcList, err := spcClient.List(metav1.ListOptions{})
-	Expect(err).To(BeNil())
-	for _, spc := range spcList.Items {
-		_, err := spcClient.Delete(spc.Name, &metav1.DeleteOptions{})
+	for _, cspc := range cspcList.Items {
+		_, err := cspcClient.Delete(cspc.Name, &metav1.DeleteOptions{})
 		Expect(err).To(BeNil())
 	}
 })
@@ -335,29 +384,28 @@ func (ops *operations) newCspClient() *operations {
 }
 
 func (ops *operations) newSpcClient() *operations {
-	newSpcClient, err := spc.KubeClient().WithFlag(KubeConfigPath)
-	Expect(err).To(BeNil())
-	ops.spcClient = newSpcClient
+	newSpcClient := cspc.NewKubeClient(cspc.WithKubeConfigPath(KubeConfigPath))
+	ops.cspcClient = newSpcClient
 	return ops
 }
 
-func (ops *operations) getHealthyCSPCount(spcName string) int {
+func (ops *operations) getHealthyCSPCount(cspcName string) int {
 	cspAPIList, err := ops.cspClient.List(metav1.ListOptions{})
 	Expect(err).To(BeNil())
 	count := csp.
 		ListBuilderForAPIObject(cspAPIList).
 		List().
-		Filter(csp.HasLabel(string(apis.StoragePoolClaimCPK), spcName), csp.IsStatus("Healthy")).Len()
+		Filter(csp.HasLabel(string(apis.CStorPoolClusterCPK), cspcName), csp.IsStatus("Healthy")).Len()
 	return count
 }
 
-func (ops *operations) deleteCSP(spcName string, deleteCount int) {
+func (ops *operations) deleteCSP(cspcName string, deleteCount int) {
 	cspAPIList, err := ops.cspClient.List(metav1.ListOptions{})
 	Expect(err).To(BeNil())
 	cspList := csp.
 		ListBuilderForAPIObject(cspAPIList).
 		List().
-		Filter(csp.HasLabel(string(apis.StoragePoolClaimCPK), spcName), csp.IsStatus("Healthy"))
+		Filter(csp.HasLabel(string(apis.CStorPoolClusterCPK), cspcName), csp.IsStatus("Healthy"))
 	cspCount := cspList.Len()
 	Expect(deleteCount).Should(BeNumerically("<=", cspCount))
 
@@ -368,12 +416,31 @@ func (ops *operations) deleteCSP(spcName string, deleteCount int) {
 	}
 }
 
-func (ops *operations) isHealthyCspCount(spcName string, expectedCspCount int) int {
+func (ops *operations) isCSPOnDifferentNodes(cspcName string) bool {
+	cspAPIList, err := ops.cspClient.List(metav1.ListOptions{})
+	Expect(err).To(BeNil())
+	cspList := csp.
+		ListBuilderForAPIObject(cspAPIList).
+		List().
+		Filter(csp.HasLabel(string(apis.CStorPoolClusterCPK), cspcName))
+	cspCountOnNode:= make(map[string]int)
+	for _,val:=range cspList.ObjectList.Items{
+		val:=val
+		cspObj:=csp.BuilderForAPIObject(&val).Csp
+		cspCountOnNode[cspObj.GetNodeName()]++
+		if cspCountOnNode[cspObj.GetNodeName()]>1{
+			return false
+		}
+	}
+	return true
+}
+
+func (ops *operations) isHealthyCspCount(cspcName string, expectedCspCount int) int {
 	var maxRetry int
 	var cspCount int
 	maxRetry = MaxRetry
 	for i := 0; i < maxRetry; i++ {
-		cspCount = ops.getHealthyCSPCount(spcName)
+		cspCount = ops.getHealthyCSPCount(cspcName)
 		if cspCount == expectedCspCount {
 			return expectedCspCount
 		}
