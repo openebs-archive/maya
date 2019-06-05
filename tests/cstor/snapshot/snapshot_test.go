e575842e79d1de7462944ccf1772bf1350022417
Prateek Pandey <prateek.pandey@mayadata.io>
2019-06-05 12:34:56 +0530

    chore(BDD): refactor cstor volume,snapshot and clone to use generate name (#1246)
    This commit refactors volume and snapshot BDD tests to use `generateName` for cluster scoped resources i.e. Namespace, SPC and StorageClass
    
    Signed-off-by: prateekpandey14 <prateek.pandey@mayadata.io>

--- tests/cstor/snapshot/snapshot_test.go
+++ tests/cstor/snapshot/snapshot_test.go
@@ -17,6 +17,9 @@ limitations under the License.
 package snapshot
 
 import (
+	"strconv"
+	"strings"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -38,20 +41,9 @@ var _ = Describe("[cstor] TEST SNAPSHOT PROVISIONING", func() {
 
 	BeforeEach(func() {
 		When("deploying cstor sparse pool", func() {
-			By("building storageclass object")
-			scObj, err = sc.NewBuilder().
-				WithName(scName).
-				WithAnnotations(annotations).
-				WithProvisioner(openebsProvisioner).Build()
-			Expect(err).ShouldNot(HaveOccurred(), "while building storageclass obj for storageclass {%s}", scName)
-
-			By("creating storageclass")
-			_, err = ops.SCClient.Create(scObj)
-			Expect(err).To(BeNil(), "while creating storageclass", scName)
-
 			By("building spc object")
 			spcObj = spc.NewBuilder().
-				WithName(spcName).
+				WithGenerateName(spcName).
 				WithDiskType(string(apis.TypeSparseCPV)).
 				WithMaxPool(cstor.PoolCount).
 				WithOverProvisioning(false).
@@ -59,12 +51,29 @@ var _ = Describe("[cstor] TEST SNAPSHOT PROVISIONING", func() {
 				Build().Object
 
 			By("creating storagepoolclaim")
-			_, err = ops.SPCClient.Create(spcObj)
+			spcObj, err = ops.SPCClient.Create(spcObj)
 			Expect(err).To(BeNil(), "while creating spc", spcName)
 
 			By("verifying healthy csp count")
-			cspCount := ops.GetHealthyCSPCount(spcName, cstor.PoolCount)
+			cspCount := ops.GetHealthyCSPCount(spcObj.Name, cstor.PoolCount)
 			Expect(cspCount).To(Equal(1), "while checking cstorpool health count")
+
+			By("building a CAS Config with generated SPC name")
+			CASConfig := strings.Replace(openebsCASConfigValue, "$spcName", spcObj.Name, 1)
+			CASConfig = strings.Replace(CASConfig, "$count", strconv.Itoa(cstor.ReplicaCount), 1)
+			annotations[string(apis.CASTypeKey)] = string(apis.CstorVolume)
+			annotations[string(apis.CASConfigKey)] = CASConfig
+
+			By("building storageclass object")
+			scObj, err = sc.NewBuilder().
+				WithGenerateName(scName).
+				WithAnnotations(annotations).
+				WithProvisioner(openebsProvisioner).Build()
+			Expect(err).ShouldNot(HaveOccurred(), "while building storageclass obj for storageclass {%s}", scName)
+
+			By("creating storageclass")
+			scObj, err = ops.SCClient.Create(scObj)
+			Expect(err).To(BeNil(), "while creating storageclass", scName)
 
 		})
 	})
@@ -72,11 +81,11 @@ var _ = Describe("[cstor] TEST SNAPSHOT PROVISIONING", func() {
 	AfterEach(func() {
 		By("deleting resources created for cstor volume snapshot provisioning", func() {
 			By("deleting storagepoolclaim")
-			_, err = ops.SPCClient.Delete(spcName, &metav1.DeleteOptions{})
+			_, err = ops.SPCClient.Delete(spcObj.Name, &metav1.DeleteOptions{})
 			Expect(err).To(BeNil(), "while deleting the spc's {%s}", spcName)
 
 			By("deleting storageclass")
-			err = ops.SCClient.Delete(scName, &metav1.DeleteOptions{})
+			err = ops.SCClient.Delete(scObj.Name, &metav1.DeleteOptions{})
 			Expect(err).To(BeNil(), "while deleting storageclass", scName)
 
 		})
@@ -88,39 +97,40 @@ var _ = Describe("[cstor] TEST SNAPSHOT PROVISIONING", func() {
 			By("building a persistentvolumeclaim")
 			pvcObj, err = pvc.NewBuilder().
 				WithName(pvcName).
-				WithNamespace(nsName).
-				WithStorageClass(scName).
+				WithNamespace(nsObj.Name).
+				WithStorageClass(scObj.Name).
 				WithAccessModes(accessModes).
 				WithCapacity(capacity).Build()
 			Expect(err).ShouldNot(
 				HaveOccurred(),
 				"while building pvc {%s} in namespace {%s}",
 				pvcName,
-				nsName,
+				nsObj.Name,
 			)
 
 			By("creating cstor persistentvolumeclaim")
-			_, err = ops.PVCClient.WithNamespace(nsName).Create(pvcObj)
+			pvcObj, err = ops.PVCClient.WithNamespace(nsObj.Name).Create(pvcObj)
 			Expect(err).To(
 				BeNil(),
 				"while creating pvc {%s} in namespace {%s}",
 				pvcName,
-				nsName,
+				nsObj.Name,
 			)
 
 			By("verifying volume target pod count as 1")
-			controllerPodCount := ops.GetPodRunningCountEventually(openebsNamespace, targetLabel, 1)
+			targetVolumeLabel := pvcLabel + pvcObj.Name
+			controllerPodCount := ops.GetPodRunningCountEventually(openebsNamespace, targetVolumeLabel, 1)
 			Expect(controllerPodCount).To(Equal(1), "while checking controller pod count")
 
 			By("verifying cstorvolume replica count")
-			pvcObj, err = ops.PVCClient.WithNamespace(nsName).Get(pvcName, metav1.GetOptions{})
+			pvcObj, err = ops.PVCClient.WithNamespace(nsObj.Name).Get(pvcName, metav1.GetOptions{})
 			Expect(err).To(
 				BeNil(),
 				"while getting pvc {%s} in namespace {%s}",
 				pvcName,
-				nsName,
+				nsObj.Name,
 			)
-			cvrLabel := "openebs.io/persistent-volume=" + pvcObj.Spec.VolumeName
+			cvrLabel := pvLabel + pvcObj.Spec.VolumeName
 			cvrCount := ops.GetCstorVolumeReplicaCountEventually(openebsNamespace, cvrLabel, cstor.ReplicaCount)
 			Expect(cvrCount).To(Equal(true), "while checking cstorvolume replica count")
 
@@ -131,23 +141,23 @@ var _ = Describe("[cstor] TEST SNAPSHOT PROVISIONING", func() {
 			By("building a cstor volume snapshot")
 			snapObj, err = snap.NewBuilder().
 				WithName(snapName).
-				WithNamespace(nsName).
+				WithNamespace(nsObj.Name).
 				WithPVC(pvcName).
 				Build()
 			Expect(err).To(
 				BeNil(),
 				"while building snapshot {%s} in namespace {%s}",
 				snapName,
-				nsName,
+				nsObj.Name,
 			)
 
 			By("creating cstor volume snapshot")
-			_, err = ops.SnapClient.WithNamespace(nsName).Create(snapObj)
+			_, err = ops.SnapClient.WithNamespace(nsObj.Name).Create(snapObj)
 			Expect(err).To(
 				BeNil(),
 				"while creating snapshot {%s} in namespace {%s}",
 				snapName,
-				nsName,
+				nsObj.Name,
 			)
 
 			By("verifying snapshot status as ready")
@@ -160,7 +170,7 @@ var _ = Describe("[cstor] TEST SNAPSHOT PROVISIONING", func() {
 				BeNil(),
 				"while deleting snapshot {%s} in namespace {%s}",
 				snapName,
-				nsName,
+				nsObj.Name,
 			)
 
 			By("verifying deleted snapshot")
@@ -173,11 +183,11 @@ var _ = Describe("[cstor] TEST SNAPSHOT PROVISIONING", func() {
 				BeNil(),
 				"while deleting pvc {%s} in namespace {%s}",
 				pvcName,
-				nsName,
+				nsObj.Name,
 			)
 
 			By("verifying target pod count as 0")
-			controllerPodCount = ops.GetPodRunningCountEventually(openebsNamespace, targetLabel, 0)
+			controllerPodCount = ops.GetPodRunningCountEventually(openebsNamespace, targetVolumeLabel, 0)
 			Expect(controllerPodCount).To(Equal(0), "while checking controller pod count")
 
 			By("verifying deleted pvc")
@@ -185,7 +195,7 @@ var _ = Describe("[cstor] TEST SNAPSHOT PROVISIONING", func() {
 			Expect(pvc).To(Equal(true), "while trying to get deleted pvc")
 
 			By("verifying if cstorvolume is deleted")
-			CstorVolumeLabel := "openebs.io/persistent-volume=" + pvcObj.Spec.VolumeName
+			CstorVolumeLabel := pvLabel + pvcObj.Spec.VolumeName
 			cvCount := ops.GetCstorVolumeCountEventually(openebsNamespace, CstorVolumeLabel, 0)
 			Expect(cvCount).To(Equal(true), "while checking cstorvolume count")
 		})
