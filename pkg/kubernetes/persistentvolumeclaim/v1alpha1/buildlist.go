b6deea3d867394c6bc585649d05980eb672f7dc2
Shubham Bajpai <shubham.bajpai@mayadata.io>
2019-06-13 09:52:23 +0530

    bdd(pvc, cstor): add bulk create and delete of pvcs in a given namespace (#1276)
    This commit adds the BDD to create PVCs in bulk and removing them in bulk as well
    
    Signed-off-by: shubham <shubham.bajpai@mayadata.io>

--- pkg/kubernetes/persistentvolumeclaim/v1alpha1/buildlist.go
+++ pkg/kubernetes/persistentvolumeclaim/v1alpha1/buildlist.go
@@ -22,8 +22,16 @@ import (
 )
 
 // ListBuilder enables building an instance of
-// PVClist
+// PVCList
 type ListBuilder struct {
+	// template to build a list of pvcs
+	template *corev1.PersistentVolumeClaim
+
+	// count determines the number of
+	// pvcs to be built using the provided
+	// template
+	count int
+
 	list    *PVCList
 	filters PredicateList
 	errs    []error
@@ -34,47 +42,104 @@ func NewListBuilder() *ListBuilder {
 	return &ListBuilder{list: &PVCList{}}
 }
 
-// ListBuilderForAPIObjects builds the ListBuilder object based on PVC api list
-func ListBuilderForAPIObjects(pvcs *corev1.PersistentVolumeClaimList) *ListBuilder {
-	b := &ListBuilder{list: &PVCList{}}
-	if pvcs == nil {
-		b.errs = append(b.errs, errors.New("failed to build pvc list: missing api list"))
+// ListBuilderFromTemplate returns a new instance of
+// ListBuilder based on the provided pvc template
+func ListBuilderFromTemplate(pvc *corev1.PersistentVolumeClaim) *ListBuilder {
+	b := NewListBuilder()
+	if pvc == nil {
+		b.errs = append(
+			b.errs,
+			errors.New("failed to build pvc list: nil pvc template"),
+		)
 		return b
 	}
+
+	b.template = pvc
+	b.count = 1
+	return b
+}
+
+// ListBuilderForAPIObjects returns a new instance of
+// ListBuilder based on provided api pvc list
+func ListBuilderForAPIObjects(pvcs *corev1.PersistentVolumeClaimList) *ListBuilder {
+	b := &ListBuilder{list: &PVCList{}}
+
+	if pvcs == nil {
+		b.errs = append(
+			b.errs,
+			errors.New("failed to build pvc list: missing api list"),
+		)
+		return b
+	}
+
 	for _, pvc := range pvcs.Items {
 		pvc := pvc
 		b.list.items = append(b.list.items, &PVC{object: &pvc})
 	}
+
 	return b
 }
 
-// ListBuilderForObjects builds the ListBuilder object based on PVCList
+// ListBuilderForObjects returns a new instance of
+// ListBuilder based on provided pvc list
 func ListBuilderForObjects(pvcs *PVCList) *ListBuilder {
 	b := &ListBuilder{}
 	if pvcs == nil {
-		b.errs = append(b.errs, errors.New("failed to build pvc list: missing object list"))
+		b.errs = append(
+			b.errs,
+			errors.New("failed to build pvc list: missing object list"),
+		)
 		return b
 	}
+
 	b.list = pvcs
 	return b
 }
 
-// List returns the list of pvc
-// instances that was built by this
-// builder
+// WithFilter adds filters on which the pvcs
+// are filtered
+func (b *ListBuilder) WithFilter(pred ...Predicate) *ListBuilder {
+	b.filters = append(b.filters, pred...)
+	return b
+}
+
+// WithCount sets the count that determines
+// the number of pvcs to be built
+func (b *ListBuilder) WithCount(count int) *ListBuilder {
+	b.count = count
+	return b
+}
+
+func (b *ListBuilder) buildFromTemplateIfNilList() {
+	if len(b.list.items) != 0 || b.template == nil {
+		return
+	}
+
+	for i := 0; i < b.count; i++ {
+		b.list.items = append(b.list.items, &PVC{object: b.template})
+	}
+}
+
+// List returns the list of pvc instances
+// that was built by this builder
 func (b *ListBuilder) List() (*PVCList, error) {
 	if len(b.errs) > 0 {
-		return nil, errors.Errorf("failed to list pvc: %+v", b.errs)
+		return nil, errors.Errorf("failed to build pvc list: %+v", b.errs)
 	}
+
+	b.buildFromTemplateIfNilList()
+
 	if b.filters == nil || len(b.filters) == 0 {
 		return b.list, nil
 	}
+
 	filteredList := &PVCList{}
 	for _, pvc := range b.list.items {
 		if b.filters.all(pvc) {
 			filteredList.items = append(filteredList.items, pvc)
 		}
 	}
+
 	return filteredList, nil
 }
 
@@ -85,6 +150,7 @@ func (b *ListBuilder) Len() (int, error) {
 	if err != nil {
 		return 0, err
 	}
+
 	return l.Len(), nil
 }
 
@@ -94,11 +160,6 @@ func (b *ListBuilder) APIList() (*corev1.PersistentVolumeClaimList, error) {
 	if err != nil {
 		return nil, err
 	}
-	return l.ToAPIList(), nil
-}
 
-// WithFilter adds filters on which the pvc's has to be filtered
-func (b *ListBuilder) WithFilter(pred ...Predicate) *ListBuilder {
-	b.filters = append(b.filters, pred...)
-	return b
+	return l.ToAPIList(), nil
 }
