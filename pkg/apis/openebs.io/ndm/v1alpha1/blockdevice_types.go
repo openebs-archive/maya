137424a4dbb5750d6d1fc3c2126b44863e950c3c
kmova <kiran.mova@mayadata.io>
2019-08-28 02:16:46 +0000

    feat(api): include NodeAttributes in NDM BD and BDC #review
    Signed-off-by: kmova <kiran.mova@mayadata.io>

--- pkg/apis/openebs.io/ndm/v1alpha1/blockdevice_types.go
+++ pkg/apis/openebs.io/ndm/v1alpha1/blockdevice_types.go
@@ -21,60 +21,151 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// DeviceSpec defines the desired state of BlockDevice
-type DeviceSpec struct {
-	// NodeAttributes has the details of the node in which this BD is attached
-	NodeAttributes  NodeAttribute       `json:"nodeAttributes"`
-	Path            string              `json:"path"`                      //Path contain devpath (e.g. /dev/sdb)
-	Capacity        DeviceCapacity      `json:"capacity"`                  //Capacity
-	Details         DeviceDetails       `json:"details"`                   //Details contains static attributes (model, serial ..)
-	ClaimRef        *v1.ObjectReference `json:"claimRef,omitempty"`        // Reference to the BDC which has claimed this BD
-	DevLinks        []DeviceDevLink     `json:"devlinks"`                  //DevLinks contains soft links of one disk
-	FileSystem      FileSystemInfo      `json:"filesystem,omitempty"`      //FileSystem contains mountpoint and filesystem type
-	Partitioned     string              `json:"partitioned"`               //BlockDevice has partions or not (YES/NO)
-	ParentDevice    string              `json:"parentDevice,omitempty"`    //ParentDevice has the UUID of the parent device
-	AggregateDevice string              `json:"aggregateDevice,omitempty"` //AggregateDevice has the UUID of the aggregate device created from this device
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +resource:path=blockDevice
+// +k8s:openapi-gen=true
+
+// BlockDevice is the Schema used to represent a BlockDevice CR
+type BlockDevice struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DeviceSpec   `json:"spec,omitempty"`
+	Status DeviceStatus `json:"status,omitempty"`
 }
 
-// NodeAttribute defines the various attributes of the node
+// DeviceSpec defines the properties and runtime status of a BlockDevice
+type DeviceSpec struct {
+	// NodeAttributes has the details of the node on which BD is attached
+	NodeAttributes NodeAttribute `json:"nodeAttributes"`
+
+	// Path contain devpath (e.g. /dev/sdb)
+	Path string `json:"path"`
+
+	// Capacity
+	Capacity DeviceCapacity `json:"capacity"`
+
+	// Details contain static attributes of BD like model,serial, and so forth
+	Details DeviceDetails `json:"details"`
+
+	// Reference to the BDC which has claimed this BD
+	ClaimRef *v1.ObjectReference `json:"claimRef,omitempty"`
+
+	// DevLinks contains soft links of a block device like
+	// /dev/by-id/...
+	// /dev/by-uuid/...
+	DevLinks []DeviceDevLink `json:"devlinks"`
+
+	// FileSystem contains mountpoint and filesystem type
+	FileSystem FileSystemInfo `json:"filesystem,omitempty"`
+
+	// Partitioned represents if BlockDevice has partions or not (YES/NO)
+	// Currently always default to NO.
+	// TODO @kmova to be implemented/deprecated
+	Partitioned string `json:"partitioned"`
+
+	// ParentDevice was intented to store the UUID of the parent
+	// Block Device as is the case for partitioned block devices.
+	//
+	// For example:
+	// /dev/sda is the parent for /dev/sdap1
+	// TODO @kmova to be implemented/deprecated
+	ParentDevice string `json:"parentDevice,omitempty"`
+
+	// AggregateDevice was intended to store the hierachical
+	// information in cases of LVM. However this is currently
+	// not implemented and may need to be re-looked into for
+	// better design.
+	// TODO @kmova to be implemented/deprecated
+	AggregateDevice string `json:"aggregateDevice,omitempty"`
+}
+
+// NodeAttribute defines the attributes of a node where
+// the block device is attached.
+//
+// Note: Prior to introducing NodeAttributes, the BD would
+// only support gathering hostname and add it as a label
+// to the BD resource.
+//
+// In some use cases, the caller has access only to node name, not
+// the hostname. node name and hostname are different in certain
+// Kubernetes clusters.
+//
+// NodeAttributes is added to contain attributes that are not
+// available on the labels like - node name, uuid, etc.
+//
+// The node attributes are helpful in querying for block devices
+// based on node attributes.  Also, adding this in the spec allows for
+// displaying in node name in the `kubectl get bd`
+//
+// TODO  @kmova @akhil
+// Capture and add nodeUUID to BD, that can help in determining
+// if the node was recreated with same node name.
 type NodeAttribute struct {
-	// NodeName is the name of the node in which the device is attached
+	// NodeName is the name of the Kubernetes node resource on which the device is attached
 	NodeName string `json:"nodeName"`
 }
 
 // DeviceCapacity defines the physical and logical size of the block device
 type DeviceCapacity struct {
-	Storage            uint64 `json:"storage"`            // blockdevice capacity in bytes
-	PhysicalSectorSize uint32 `json:"physicalSectorSize"` // blockdevice physical-Sector size in bytes
-	LogicalSectorSize  uint32 `json:"logicalSectorSize"`  // blockdevice logical-sector size in bytes
+	// Storage is the blockdevice capacity in bytes
+	Storage uint64 `json:"storage"`
+
+	// PhysicalSectorSize is blockdevice physical-Sector size in bytes
+	PhysicalSectorSize uint32 `json:"physicalSectorSize"`
+
+	// LogicalSectorSize is blockdevice logical-sector size in bytes
+	LogicalSectorSize uint32 `json:"logicalSectorSize"`
 }
 
 // DeviceDetails represent certain hardware/static attributes of the block device
 type DeviceDetails struct {
-	DeviceType       string `json:"deviceType"`       // DeviceType represents the type of drive like SSD, HDD etc.,
-	Model            string `json:"model"`            // Model is model of disk
-	Compliance       string `json:"compliance"`       // Implemented standards/specifications version such as SPC-1, SPC-2, etc
-	Serial           string `json:"serial"`           // Serial is serial no of disk
-	Vendor           string `json:"vendor"`           // Vendor is vendor of disk
-	FirmwareRevision string `json:"firmwareRevision"` // disk firmware revision
+	// DeviceType represents the type of drive like SSD, HDD etc.,
+	DeviceType string `json:"deviceType"`
+
+	// Model is model of disk
+	Model string `json:"model"`
+
+	// Compliance is standards/specifications version implmented by device firmware
+	//  such as SPC-1, SPC-2, etc
+	Compliance string `json:"compliance"`
+
+	// Serial is serial number of disk
+	Serial string `json:"serial"`
+
+	// Vendor is vendor of disk
+	Vendor string `json:"vendor"`
+
+	// disk firmware revision
+	FirmwareRevision string `json:"firmwareRevision"`
 }
 
 // FileSystemInfo defines the filesystem type and mountpoint of the device if it exists
 type FileSystemInfo struct {
-	Type       string `json:"fsType,omitempty"`     //Type represents the FileSystem type of the block device
-	Mountpoint string `json:"mountPoint,omitempty"` //MountPoint represents the mountpoint of the block device.
+	//Type represents the FileSystem type of the block device
+	Type string `json:"fsType,omitempty"`
+
+	//MountPoint represents the mountpoint of the block device.
+	Mountpoint string `json:"mountPoint,omitempty"`
 }
 
 // DeviceDevLink holds the maping between type and links like by-id type or by-path type link
 type DeviceDevLink struct {
-	Kind  string   `json:"kind,omitempty"`  // Kind is the type of link like by-id or by-path.
-	Links []string `json:"links,omitempty"` // Links are the soft links of Type type
+	// Kind is the type of link like by-id or by-path.
+	Kind string `json:"kind,omitempty"`
+
+	// Links are the soft links of Type type
+	Links []string `json:"links,omitempty"`
 }
 
 // DeviceStatus defines the observed state of BlockDevice
 type DeviceStatus struct {
-	ClaimState DeviceClaimState `json:"claimState"` // claim state of the block device
-	State      string           `json:"state"`      // current state of the blockdevice (Active/Inactive)
+	// claim state of the block device
+	ClaimState DeviceClaimState `json:"claimState"`
+
+	// current state of the blockdevice (Active/Inactive)
+	State string `json:"state"`
 }
 
 // DeviceClaimState defines the observed state of BlockDevice
@@ -84,26 +175,14 @@ const (
 	// BlockDeviceUnclaimed represents that the block device is not bound to any BDC,
 	// all cleanup jobs have been completed and is available for claiming.
 	BlockDeviceUnclaimed DeviceClaimState = "Unclaimed"
+
 	// BlockDeviceReleased represents that the block device is released from the BDC,
 	// pending cleanup jobs
 	BlockDeviceReleased DeviceClaimState = "Released"
+
 	// BlockDeviceClaimed represents that the block device is bound to a BDC
 	BlockDeviceClaimed DeviceClaimState = "Claimed"
 )
-
-// +genclient
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// +resource:path=blockDevice
-// +k8s:openapi-gen=true
-
-// BlockDevice is the Schema for the devices API
-type BlockDevice struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   DeviceSpec   `json:"spec,omitempty"`
-	Status DeviceStatus `json:"status,omitempty"`
-}
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +resource:path=blockDeviceList
