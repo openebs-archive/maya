1eafa7d77575d150cdfd8daf3b09bd54f08c128e
Payes Anand <payes.anand@mayadata.io>
2019-09-03 11:19:45 +0000

    Reorganize tests
    Signed-off-by: Payes Anand <payes.anand@mayadata.io>

--- tests/csi/cstor/volume/resize_test.go
+++ tests/csi/cstor/volume/resize_test.go
@@ -16,38 +16,36 @@ limitations under the License.
 
 package volume
 
-import (
-	. "github.com/onsi/ginkgo"
-)
+import . "github.com/onsi/ginkgo"
 
 var _ = Describe("[cstor] [sparse] TEST VOLUME PROVISIONING WITH APP POD RESTART", func() {
-	BeforeEach(prepareForVolumeCreationTest)
-	AfterEach(cleanupAfterVolumeCreationTest)
+	BeforeEach(prepareForVolumeResizeTest)
+	AfterEach(cleanupAfterVolumeResizeTest)
 
-	When("app is deployed and restarted on pvc with replica count 1", volumeCreationTest)
+	When("app is deployed and restarted on pvc with replica count 1", volumeResizeTest)
 })
 
-func volumeCreationTest() {
-	When("volumeCreationTest", func() {
+func volumeResizeTest() {
+	When("volumeResizeTest", func() {
 		It("should crete and verify PVC bound status", CreateAndVerifyPVC)
 		It("should crete and deploy app pod", CreateAndDeployApp)
 		It("should verify presence of components related to volume", VerifyVolumeComponents)
-		It("should restart app pod and verify running status", restartAppPodAndVerifyRunningStatus)
+		It("should expand PVC", expandPVC)
 		It("should delete application deployment", deleteAppDeployment)
 		It("should delete pvc", deletePVC)
 		It("should verify volume components deletion", verifyVolumeComponentsDeletion)
 	})
 }
 
-func prepareForVolumeCreationTest() {
-	When("prepareForVolumeCreationTest", func() {
+func prepareForVolumeResizeTest() {
+	When("prepareForVolumeResizeTest", func() {
 		By("should create and verify cstorpoolcluster", createAndVerifyCstorPoolCluster)
 		By("should create storage class", createStorageClass)
 	})
 }
 
-func cleanupAfterVolumeCreationTest() {
-	When("cleanupAfterVolumeCreationTest", func() {
+func cleanupAfterVolumeResizeTest() {
+	When("cleanupAfterVolumeResizeTest", func() {
 		By("should delete cstorpoolcluster", deleteCstorPoolCluster)
 		By("should delete storage class", deleteStorageClass)
 	})
