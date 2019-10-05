159e6a96c6e2fd8fbc049abd060cdcc0f8a49084
wenzdey <56051809+wenzdey@users.noreply.github.com>
2019-10-05 12:08:31 -0500

    Fixed golint issue in doc.go #2148
    https://github.com/openebs/openebs/issues/2148
    
    Signed-off-by: wenzdey <56051809+wenzdey@users.noreply.github.com>

--- cmd/maya-agent/types/doc.go
+++ cmd/maya-agent/types/doc.go
@@ -14,5 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Common types in the maya-agent
+// Package types contains common types in the maya-agent.
 package types
