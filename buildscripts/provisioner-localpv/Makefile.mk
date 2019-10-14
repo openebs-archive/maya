f6d331c63df7621728fc4f69641af1eaa71b4314
Kiran Mova <kiran.mova@mayadata.io>
2019-10-14 21:10:34 +0530

    refactor(build): setup arm builds for apiserver and local provisioner (#1477)
    * refactor(build): setup arm builds for apiserver and local provisioner
    
    Modularize the Makefile by pushing the mayactl, apiserver targets
    to their specific folders in order to make it easier to modify/enhance
    and for better readability.
    
    Added the build check for arm64 as well as added Dockerfile for
    creating arm64 based apiserver and local provisioner.
    
    Signed-off-by: kmova <kiran.mova@mayadata.io>

--- buildscripts/provisioner-localpv/Makefile.mk
+++ buildscripts/provisioner-localpv/Makefile.mk
@@ -3,16 +3,27 @@
 PROVISIONER_LOCALPV=provisioner-localpv
 
 #Use this to build provisioner-localpv
+.PHONY: provisioner-localpv
 provisioner-localpv:
 	@echo "----------------------------"
 	@echo "--> provisioner-localpv    "
 	@echo "----------------------------"
 	@PNAME=${PROVISIONER_LOCALPV} CTLNAME=${PROVISIONER_LOCALPV} sh -c "'$(PWD)/buildscripts/build.sh'"
 
+.PHONY: provisioner-localpv-image
 provisioner-localpv-image: provisioner-localpv
 	@echo "-------------------------------"
 	@echo "--> provisioner-localpv image "
 	@echo "-------------------------------"
 	@cp bin/provisioner-localpv/${PROVISIONER_LOCALPV} buildscripts/provisioner-localpv/
 	@cd buildscripts/provisioner-localpv && sudo docker build -t openebs/provisioner-localpv:${IMAGE_TAG} --build-arg BUILD_DATE=${BUILD_DATE} . --no-cache
+	@rm buildscripts/provisioner-localpv/${PROVISIONER_LOCALPV}
+
+.PHONY: provisioner-localpv-image.arm64
+provisioner-localpv-image.arm64: provisioner-localpv
+	@echo "-------------------------------"
+	@echo "--> provisioner-localpv image "
+	@echo "-------------------------------"
+	@cp bin/provisioner-localpv/${PROVISIONER_LOCALPV} buildscripts/provisioner-localpv/
+	@cd buildscripts/provisioner-localpv && sudo docker build -t openebs/provisioner-localpv-arm64:${IMAGE_TAG} -f Dockerfile.arm64 --build-arg BUILD_DATE=${BUILD_DATE} --build-arg BASE_IMAGE=${BASE_DOCKER_IMAGEARM64} . --no-cache
 	@rm buildscripts/provisioner-localpv/${PROVISIONER_LOCALPV}
