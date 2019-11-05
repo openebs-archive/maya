f5ac6bc85fab578106df4f72ae7130d19c246679
shubham <shubham.bajpai@mayadata.io>
2019-11-05 16:50:47 +0530

    updated valid current version
    Signed-off-by: shubham <shubham.bajpai@mayadata.io>

--- pkg/apis/openebs.io/v1alpha1/versionDetails.go
+++ pkg/apis/openebs.io/v1alpha1/versionDetails.go
@@ -25,7 +25,7 @@ import (
 
 var (
 	validCurrentVersions = map[string]bool{
-		"1.0.0": true, "1.1.0": true, "1.2.0": true,
+		"1.0.0": true, "1.1.0": true, "1.2.0": true, "1.3.0": true,
 	}
 	validDesiredVersion = version.GetVersion()
 )
