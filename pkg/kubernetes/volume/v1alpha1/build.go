e305a21228e936249a4baa506c00bc1dc2c0c3a9
Rahul M Chheda <rahul.chheda@mayadata.io>
2019-11-22 13:24:12 +0530

    Added the functionality to set the DefaultMode for Volumes
    Signed-off-by: Rahul M Chheda <rahul.chheda@mayadata.io>

--- pkg/kubernetes/volume/v1alpha1/build.go
+++ pkg/kubernetes/volume/v1alpha1/build.go
@@ -64,7 +64,7 @@ func (b *Builder) WithHostDirectory(path string) *Builder {
 	b.volume.object.VolumeSource = volumeSource
 	return b
 }
-func (b *Builder) WithConfigMap(configMap *corev1.ConfigMap) *Builder {
+func (b *Builder) WithConfigMap(configMap *corev1.ConfigMap, defaultmode int32) *Builder {
 	if configMap == nil {
 		b.errs = append(
 			b.errs,
@@ -72,10 +72,15 @@ func (b *Builder) WithConfigMap(configMap *corev1.ConfigMap) *Builder {
 		)
 		return b
 	}
-	k := int32(420)
+	if defaultmode == 0 {
+		b.errs = append(
+			b.errs,
+			errors.New("failed to build volume object: missing defaultmode"),
+		)
+	}
 	volumeSource := corev1.VolumeSource{
 		ConfigMap: &corev1.ConfigMapVolumeSource{
-			DefaultMode: &k,
+			DefaultMode: &defaultmode,
 			LocalObjectReference: corev1.LocalObjectReference{
 				Name: configMap.Name,
 			},
