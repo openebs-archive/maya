3a4a876cf4833ef8c05d11351831be6799b14285
shubham <shubham.bajpai@mayadata.io>
2020-03-11 16:06:48 +0530

    fixed typo
    Signed-off-by: shubham <shubham.bajpai@mayadata.io>

--- pkg/upgrade/upgrader/jiva_upgrade.go
+++ pkg/upgrade/upgrader/jiva_upgrade.go
@@ -437,7 +437,7 @@ func scaleDownTargetDeploy(name, namespace string) error {
 		return err
 	}
 	podList := &corev1.PodList{}
-	// Wait for up to 5 minutes targe pod to go away.
+	// Wait for up to 5 minutes for target pod to go away.
 	for i := 0; i < 60; i++ {
 		podList, err = podClient.WithNamespace(namespace).List(
 			metav1.ListOptions{
