4363acfbf1859562f19a6848edd360a1b723e37d
Niladri Halder <niladri.halder@mayadata.io>
2021-08-02 10:36:33 +0530

    Add version bump
    Signed-off-by: Niladri Halder <niladri.halder@mayadata.io>

--- pkg/apis/openebs.io/v1alpha1/versionDetails.go
+++ pkg/apis/openebs.io/v1alpha1/versionDetails.go
@@ -31,7 +31,7 @@ var (
 		"1.12.0": true, "2.0.0": true, "2.1.0": true, "2.2.0": true,
 		"2.3.0": true, "2.4.0": true, "2.4.1": true, "2.5.0": true,
 		"2.6.0": true, "2.7.0": true, "2.8.0": true, "2.9.0": true,
-		"2.10.0": true, "2.11.0": true,
+		"2.10.0": true, "2.11.0": true, "2.12.0": true,
 	}
 	validDesiredVersion = strings.Split(version.GetVersion(), "-")[0]
 )
